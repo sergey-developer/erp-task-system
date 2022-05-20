61c9c4e26c2cf20641bc024052cc0ffb05c6d18d
sergey-developer <ijrtre123@gmail.com>
2022-05-20 12:09:29 +0600

    Поправил код.

--- src/state/rootReducer.ts
+++ src/state/rootReducer.ts
@@ -1,9 +1,7 @@
 import authReducer from 'modules/auth/authSlice'
-import tasksReducer from 'modules/tasks/tasksSlice'
 import { api } from 'shared/services/api'
 
 export const rootReducer = {
   [api.reducerPath]: api.reducer,
-  authReducer,
-  tasksReducer,
+  auth: authReducer,
 }
