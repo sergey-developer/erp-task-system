3f2b8a1fb5bee12f9a7a9fd52ae6883d596978ac
sergey-developer <ijrtre123@gmail.com>
2022-05-21 20:02:57 +0600

    Поправил стили.

--- src/components/Layout/PrivateLayout/styles.ts
+++ src/components/Layout/PrivateLayout/styles.ts
@@ -4,8 +4,7 @@ import styled from 'styled-components'
 
 export const ContentStyled = styled(Layout.Content)`
   padding: 32px 40px;
-  height: calc(100vh - 60px);
-  overflow-y: hidden;
+  min-height: calc(100vh - 60px);
   display: flex;
   flex-direction: column;
   flex-flow: column;
