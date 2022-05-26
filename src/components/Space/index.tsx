b84b6c4f8145176698b0106fa0d920333ef2adb9
sergey-developer <ijrtre123@gmail.com>
2022-05-26 18:35:27 +0600

    Добавил описание для компонента Space.

--- src/components/Space/index.tsx
+++ src/components/Space/index.tsx
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 
 type SpaceProps = { $fullWidth?: boolean }
 
+/**
+ * Нужен для того, чтобы компонент Space из antd расширялся на ширину своего родителя
+ */
+
 const Space = styled(BaseSpace)<SpaceProps>`
   ${({ $fullWidth }) => ($fullWidth ? 'width: 100%' : '')};
 `
