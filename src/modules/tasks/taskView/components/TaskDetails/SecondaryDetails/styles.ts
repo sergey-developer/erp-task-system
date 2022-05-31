94232897347ca999b367d7f67e702fe49144c2e1
sergey-developer <ijrtre123@gmail.com>
2022-05-31 14:06:35 +0600

    Поправил стили для селекта.

--- src/modules/tasks/taskView/components/TaskDetails/SecondaryDetails/styles.ts
+++ src/modules/tasks/taskView/components/TaskDetails/SecondaryDetails/styles.ts
@@ -5,8 +5,12 @@ import styled from 'styled-components'
 export const SelectStyled = styled(Select)`
   width: 100%;
 
-  .ant-select-selector {
-    height: max-content !important;
-    padding-left: 0 !important;
+  &&.ant-select .ant-select-selector {
+    height: max-content;
+    padding-left: 0;
+  }
+
+  &.ant-select-single .ant-select-selector .ant-select-selection-item {
+    line-height: unset;
   }
 `
