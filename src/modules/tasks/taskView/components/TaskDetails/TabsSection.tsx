645177891ba475bacf7cbabd6e1967ba4e689c69
Maxxim Efimov <maddmaxx2000@mail.ru>
2022-06-28 08:17:48 +0300

    интеграция решения с бекЭнд

--- src/modules/tasks/taskView/components/TaskDetails/TabsSection.tsx
+++ src/modules/tasks/taskView/components/TaskDetails/TabsSection.tsx
@@ -1,12 +1,21 @@
 import { Tabs, Typography } from 'antd'
 import React, { FC } from 'react'
 
-import ImagesBlock from './ImagesBlock'
+import { TaskTypeEnum } from 'modules/tasks/constants'
+
 import { DetailContainerStyled } from './styles'
 
 const { TabPane } = Tabs
-
-const TabsSection: FC = () => {
+export type TabsSectionProps = {
+  techResolution?: string
+  userResolution?: string
+  type?: TaskTypeEnum
+}
+const TabsSection: FC<TabsSectionProps> = ({
+  techResolution,
+  userResolution,
+  type,
+}) => {
   return (
     <DetailContainerStyled>
       <Tabs defaultActiveKey='1'>
@@ -15,27 +24,32 @@ const TabsSection: FC = () => {
         </TabPane>
         <TabPane tab='Решение' key='2'>
           <Typography.Title level={5}>Решение</Typography.Title>
-          <Typography.Text type='secondary'>
-            Техническое решение
-          </Typography.Text>
-          <Typography.Paragraph>
-            Касса Вики Принт 57Ф, прошивка последняя. Через драйвер все работает
-            — открытие/закрытие смены.
-          </Typography.Paragraph>
-          <Typography.Text type='secondary'>
-            Решение для пользователя
-          </Typography.Text>
-          <Typography.Paragraph>
-            Касса Вики Принт 57Ф, прошивка последняя. Через драйвер все работает
-            — открытие/закрытие смены.
-          </Typography.Paragraph>
-          <ImagesBlock
-            images={[
-              'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-              'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-              'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-            ]}
-          />
+          {techResolution || userResolution ? (
+            <>
+              {techResolution && (
+                <>
+                  <Typography.Text type='secondary'>
+                    Техническое решение
+                  </Typography.Text>
+                  <Typography.Paragraph>{techResolution}</Typography.Paragraph>
+                </>
+              )}
+              {userResolution &&
+                type !== TaskTypeEnum.Incident &&
+                type !== TaskTypeEnum.Request && (
+                  <>
+                    <Typography.Text type='secondary'>
+                      Решение для пользователя
+                    </Typography.Text>
+                    <Typography.Paragraph>
+                      {userResolution}
+                    </Typography.Paragraph>
+                  </>
+                )}
+            </>
+          ) : (
+            '-'
+          )}
         </TabPane>
         <TabPane tab='Задания' key='3'>
           Задания
