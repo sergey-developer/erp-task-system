996f7e41f9256159fe78a4ce8b2728614a392287
Maxxim Efimov <maddmaxx2000@mail.ru>
2022-06-29 14:38:38 +0300

    resolve PR comments

--- src/modules/tasks/taskView/components/TaskDetails/TabsSection.tsx
+++ src/modules/tasks/taskView/components/TaskDetails/TabsSection.tsx
@@ -1,5 +1,5 @@
 import { Tabs, Typography } from 'antd'
-import React, { FC } from 'react'
+import React, { FC, useMemo } from 'react'
 
 import { TaskTypeEnum } from 'modules/tasks/constants'
 
@@ -16,6 +16,30 @@ const TabsSection: FC<TabsSectionProps> = ({
   userResolution,
   type,
 }) => {
+  const techResolutionContent = useMemo(() => {
+    return techResolution ? (
+      <>
+        <Typography.Text type='secondary'>Техническое решение</Typography.Text>
+        <Typography.Paragraph>{techResolution}</Typography.Paragraph>
+      </>
+    ) : null
+  }, [techResolution])
+
+  const userResolutionContent = useMemo(() => {
+    return userResolution &&
+      type !== TaskTypeEnum.Incident &&
+      type !== TaskTypeEnum.Request ? (
+      <>
+        <Typography.Text type='secondary'>
+          Решение для пользователя
+        </Typography.Text>
+        <Typography.Paragraph>{userResolution}</Typography.Paragraph>
+      </>
+    ) : null
+  }, [type, userResolution])
+
+  const showResolution = !!techResolutionContent || !!userResolutionContent
+
   return (
     <DetailContainerStyled>
       <Tabs defaultActiveKey='1'>
@@ -24,28 +48,10 @@ const TabsSection: FC<TabsSectionProps> = ({
         </TabPane>
         <TabPane tab='Решение' key='2'>
           <Typography.Title level={5}>Решение</Typography.Title>
-          {techResolution || userResolution ? (
+          {showResolution ? (
             <>
-              {techResolution && (
-                <>
-                  <Typography.Text type='secondary'>
-                    Техническое решение
-                  </Typography.Text>
-                  <Typography.Paragraph>{techResolution}</Typography.Paragraph>
-                </>
-              )}
-              {userResolution &&
-                type !== TaskTypeEnum.Incident &&
-                type !== TaskTypeEnum.Request && (
-                  <>
-                    <Typography.Text type='secondary'>
-                      Решение для пользователя
-                    </Typography.Text>
-                    <Typography.Paragraph>
-                      {userResolution}
-                    </Typography.Paragraph>
-                  </>
-                )}
+              {techResolution}
+              {userResolution}
             </>
           ) : (
             '-'
