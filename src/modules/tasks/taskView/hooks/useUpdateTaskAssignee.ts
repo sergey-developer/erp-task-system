1dac990aee19bd6105ddbd983c18df9ce6eca9f6
sergey-developer <ijrtre123@gmail.com>
2022-07-23 19:10:17 +0300

    Поправил обработку ошибки.

--- src/modules/tasks/taskView/hooks/useUpdateTaskAssignee.ts
+++ src/modules/tasks/taskView/hooks/useUpdateTaskAssignee.ts
@@ -2,11 +2,8 @@ import { useCallback } from 'react'
 
 import { useUpdateTaskAssigneeMutation } from 'modules/tasks/services/tasks.service'
 import useUserRole from 'modules/user/hooks/useUserRole'
-import showErrorNotification from 'shared/utils/notifications/showErrorNotification'
 
 import { UpdateTaskAssigneeMutationArgsModel } from '../models'
-
-const UPDATE_ASSIGNEE_ERROR_MSG = 'Невозможно изменить исполнителя'
 
 const useUpdateTaskAssignee = () => {
   const [mutation, state] = useUpdateTaskAssigneeMutation()
@@ -29,12 +26,7 @@ const useUpdateTaskAssignee = () => {
     async (data: UpdateTaskAssigneeMutationArgsModel) => {
       if (shouldSkip) return
 
-      try {
-        await mutation(data).unwrap()
-      } catch (error) {
-        showErrorNotification(UPDATE_ASSIGNEE_ERROR_MSG)
-        throw error
-      }
+      await mutation(data).unwrap()
     },
     [mutation, shouldSkip],
   )
