e25c3775f37487512875959c11e52c9ddf00f6cf
sergey-developer <ijrtre123@gmail.com>
2022-08-08 12:39:03 +0300

    Доработал хук useLogin.

---
+++ src/modules/auth/hooks/useLogin.ts
@@ -0,0 +1,35 @@
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { RoutesEnum } from 'configs/routes'
+import { login as loginAction } from 'modules/auth/authSlice'
+import { SignInFormFields } from 'modules/auth/components/SignIn/components/SignInPage/interfaces'
+import { IUseLoginMutationResult } from 'modules/auth/interfaces'
+import { useLoginMutation } from 'modules/auth/services/authApi.service'
+import authLocalStorageService from 'modules/auth/services/authLocalStorage.service'
+import parseJwt from 'modules/auth/utils/parseJwt'
+import useDispatch from 'shared/hooks/useDispatch'
+
+const useLogin = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [mutation, state] = useLoginMutation<IUseLoginMutationResult>()
+
+  const fn = useCallback(
+    async (fields: SignInFormFields) => {
+      // todo: добавить обработку ошибок
+      const response = await mutation(fields).unwrap()
+
+      authLocalStorageService.setAccessToken(response.access)
+      authLocalStorageService.setRefreshToken(response.refresh)
+
+      dispatch(loginAction({ user: parseJwt(response.access), ...response }))
+      navigate(RoutesEnum.Root)
+    },
+    [dispatch, mutation, navigate],
+  )
+
+  return { fn, state }
+}
+
+export default useLogin
