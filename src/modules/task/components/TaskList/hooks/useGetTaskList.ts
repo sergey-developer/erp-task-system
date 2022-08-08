5f4693e6d0e0b7f4acad239b785297409415125c
sergey-developer <ijrtre123@gmail.com>
2022-08-08 12:59:22 +0300

    Убрал лишние комменты. Оставил один нужный коммент.

--- src/modules/task/components/TaskList/hooks/useGetTaskList.ts
+++ src/modules/task/components/TaskList/hooks/useGetTaskList.ts
@@ -1,15 +1,10 @@
-import {
-  UseGetTaskListQueryReturnType,
-  useGetTaskListQuery,
-} from 'modules/task/services/taskApi.service'
+import { useGetTaskListQuery } from 'modules/task/services/taskApi.service'
 import useUserPermissions from 'modules/user/hooks/useUserPermissions'
 
 import { GetTaskListQueryArgsModel } from '../models'
 import { taskListApiPermissions } from '../permissions/taskList.permissions'
 
-const useGetTaskList = (
-  filter: GetTaskListQueryArgsModel,
-): UseGetTaskListQueryReturnType => {
+const useGetTaskList = (filter: GetTaskListQueryArgsModel) => {
   const permissions = useUserPermissions(taskListApiPermissions.getList)
 
   return useGetTaskListQuery(filter, {
--- src/modules/task/services/taskApi.service.ts
+++ src/modules/task/services/taskApi.service.ts
@@ -99,20 +99,3 @@ export const {
 } = taskApiService
 
 export default taskApiService
-
-/**
- * todo: Внимательно следить за обновлениями RTK query и поправить при первой возможности
- * RTK query не умеет правильно доставать ReturnType из сгенерированного useQuery хука
- * хак, который исправляет проблему взят отсюда https://github.com/reduxjs/redux-toolkit/issues/1363
- * открытый issue по проблеме https://github.com/reduxjs/redux-toolkit/issues/1937
- * пулл реквест за которым нужно следить https://github.com/reduxjs/redux-toolkit/pull/2276
- */
-
-let getTaskList
-if (false as boolean) {
-  // @ts-ignore
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  getTaskList = useGetTaskListQuery()
-}
-
-export type UseGetTaskListQueryReturnType = NonNullable<typeof getTaskList>
