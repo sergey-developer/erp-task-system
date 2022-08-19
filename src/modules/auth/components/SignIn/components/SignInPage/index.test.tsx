a1471d629ce3145fe4672bca56e44856f5954c7a
sergey-developer <ijrtre123@gmail.com>
2022-08-19 20:02:13 +0300

    Написал несколько тестов.

---
+++ src/modules/auth/components/SignIn/components/SignInPage/index.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import { RoutesEnum } from 'configs/routes'
+import { REQUIRED_FIELD_MSG } from 'shared/constants/messages'
+import { render, renderInRoute, screen, within } from 'tests/test-utils'
+
+import SignInPage from './index'
+
+describe('Страница авторизации', () => {
+  describe('Если не заполнить обязательные поля и нажать кнопку submit', () => {
+    test('Пользователь остаётся на странице авторизации', async () => {
+      const { user } = renderInRoute(<SignInPage />, RoutesEnum.SignIn)
+      const submitBtn = screen.getByTestId('btn-submit')
+      await user.click(submitBtn)
+      expect(window.location.pathname).toBe(RoutesEnum.SignIn)
+    })
+
+    test('Появляются ошибки обязательных полей', async () => {
+      const { user } = render(<SignInPage />)
+
+      const emailField = screen.getByTestId('field-email')
+      const passwordField = screen.getByTestId('field-password')
+      const submitBtn = screen.getByTestId('btn-submit')
+
+      await user.click(submitBtn)
+
+      expect(
+        await within(emailField).findByText(REQUIRED_FIELD_MSG),
+      ).toBeInTheDocument()
+
+      expect(
+        await within(passwordField).findByText(REQUIRED_FIELD_MSG),
+      ).toBeInTheDocument()
+    })
+  })
+})
--- src/shared/constants/validation.ts
+++ src/shared/constants/validation.ts
@@ -1,8 +1,10 @@
 import { Rule } from 'rc-field-form/es/interface'
 
+import { REQUIRED_FIELD_MSG } from 'shared/constants/messages'
+
 export const REQUIRED_FIELD_RULE: Rule = {
   required: true,
-  message: 'Обязательное поле.',
+  message: REQUIRED_FIELD_MSG,
 }
 
 export const TEXT_MAX_LENGTH_MSG =
