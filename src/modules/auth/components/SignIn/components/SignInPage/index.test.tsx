53d2ef8838a02f86bf4afa22cfbf0d78820f0f7c
sergey-developer <ijrtre123@gmail.com>
2022-08-23 21:01:36 +0300

    Добавил тест остаётся ли пользователь на странице авторизации.

--- src/modules/auth/components/SignIn/components/SignInPage/index.test.tsx
+++ src/modules/auth/components/SignIn/components/SignInPage/index.test.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 import { RoutesEnum } from 'configs/routes'
 import { REQUIRED_FIELD_MSG } from 'shared/constants/messages'
-import { render, renderInRoute, screen, within } from 'tests/test-utils'
+import {
+  render,
+  renderInRoute,
+  screen,
+  waitFor,
+  within,
+} from 'tests/test-utils'
 
 import SignInPage from './index'
 
@@ -10,8 +16,24 @@ describe('Страница авторизации', () => {
   describe('Если не заполнить обязательные поля и нажать кнопку submit', () => {
     test('Пользователь остаётся на странице авторизации', async () => {
       const { user } = renderInRoute(<SignInPage />, RoutesEnum.SignIn)
+
+      const checkHasValidatingClass = (className: string): boolean =>
+        className.includes('ant-form-item-is-validating')
+
+      const emailField = screen.getByTestId('field-email')
+      const passwordField = screen.getByTestId('field-password')
       const submitBtn = screen.getByTestId('btn-submit')
+
       await user.click(submitBtn)
+
+      await waitFor(() => {
+        expect(checkHasValidatingClass(emailField.className)).toBe(false)
+      })
+
+      await waitFor(() => {
+        expect(checkHasValidatingClass(passwordField.className)).toBe(false)
+      })
+
       expect(window.location.pathname).toBe(RoutesEnum.SignIn)
     })
 
