f621af64596538442eeda343eb14c01c99f0625f
sergey-developer <ijrtre123@gmail.com>
2022-08-25 16:58:20 +0300

    Поменял ссылку на переменную от куда читается переменная окружения.

--- src/configs/env/common.config.ts
+++ src/configs/env/common.config.ts
@@ -3,7 +3,7 @@ import { ConfigType } from './interfaces'
 export type CommonKeysUnion = 'env' | 'apiUrl'
 
 const commonConfig: ConfigType = {
-  env: process.env.NODE_ENV,
+  env: process.env.REACT_APP_ENVIRONMENT,
   apiUrl: process.env.REACT_APP_API_URL,
 }
 
