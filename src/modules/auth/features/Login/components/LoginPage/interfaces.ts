d38ad4f6224db690b72c6a31ef5609312612ac17
sergey-developer <ijrtre123@gmail.com>
2022-08-29 18:10:59 +0300

    Добавил обработку ошибок с апи.

--- src/modules/auth/features/Login/components/LoginPage/interfaces.ts
+++ src/modules/auth/features/Login/components/LoginPage/interfaces.ts
@@ -1,3 +1,6 @@
 import { LoginMutationArgsModel } from 'modules/auth/models'
+import { FieldsErrors } from 'shared/services/api'
 
 export type LoginFormFields = LoginMutationArgsModel
+
+export type LoginFormErrors = FieldsErrors<LoginFormFields>
