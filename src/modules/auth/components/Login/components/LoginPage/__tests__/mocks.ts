6b4d4a5305aab33fc6af41df172fe152b3b69803
sergey-developer <ijrtre123@gmail.com>
2022-08-29 11:24:32 +0300

    Поправил моки.

--- src/modules/auth/components/Login/components/LoginPage/__tests__/mocks.ts
+++ src/modules/auth/components/Login/components/LoginPage/__tests__/mocks.ts
@@ -1,4 +1,4 @@
-import { rest } from 'msw'
+import { MockedRequest, ResponseResolver, RestContext, rest } from 'msw'
 
 import api, { API_RESPONSE_DELAY } from '__tests__/mocks/api'
 import { HttpStatusCodeEnum } from 'shared/constants/http'
@@ -6,50 +6,44 @@ import { makeAbsoluteApiUrl } from 'shared/services/api'
 
 import { successLoginResponse } from './constants'
 
-export const mockLoginSuccess = () => {
-  api.use(
-    rest.post(makeAbsoluteApiUrl('/user/auth'), (req, res, ctx) => {
-      return res.once(
-        ctx.status(HttpStatusCodeEnum.Ok),
-        ctx.json(successLoginResponse),
-        ctx.delay(API_RESPONSE_DELAY),
-      )
-    }),
-  )
-}
+const mockLogin =
+  (mock: ResponseResolver<MockedRequest, RestContext>) => () => {
+    api.use(rest.post(makeAbsoluteApiUrl('/user/auth'), mock))
+  }
 
-export const mockLoginBadRequestError = () => {
-  api.use(
-    rest.post(makeAbsoluteApiUrl('/user/auth'), (req, res, ctx) => {
-      return res.once(
-        ctx.status(HttpStatusCodeEnum.BadRequest),
-        ctx.delay(API_RESPONSE_DELAY),
-      )
-    }),
+export const mockLoginSuccess = mockLogin((req, res, ctx) => {
+  return res.once(
+    ctx.status(HttpStatusCodeEnum.Ok),
+    ctx.json(successLoginResponse),
+    ctx.delay(API_RESPONSE_DELAY),
   )
-}
+})
+
+export const mockLoginBadRequestError = mockLogin((req, res, ctx) => {
+  return res.once(
+    ctx.status(HttpStatusCodeEnum.BadRequest),
+    ctx.delay(API_RESPONSE_DELAY),
+  )
+})
 
 export const mockLoginUnauthorizedError = () => {
+  mockLogin((req, res, ctx) => {
+    return res.once(
+      ctx.status(HttpStatusCodeEnum.Unauthorized),
+      ctx.delay(API_RESPONSE_DELAY),
+    )
+  })()
+
   api.use(
-    rest.post(makeAbsoluteApiUrl('/user/auth'), (req, res, ctx) => {
-      return res.once(
-        ctx.status(HttpStatusCodeEnum.Unauthorized),
-        ctx.delay(API_RESPONSE_DELAY),
-      )
-    }),
     rest.post(makeAbsoluteApiUrl('/user/refresh'), (req, res, ctx) => {
       return res.once(ctx.status(HttpStatusCodeEnum.Ok))
     }),
   )
 }
 
-export const mockLoginServerError = () => {
-  api.use(
-    rest.post(makeAbsoluteApiUrl('/user/auth'), (req, res, ctx) => {
-      return res.once(
-        ctx.status(HttpStatusCodeEnum.ServerError),
-        ctx.delay(API_RESPONSE_DELAY),
-      )
-    }),
+export const mockLoginServerError = mockLogin((req, res, ctx) => {
+  return res.once(
+    ctx.status(HttpStatusCodeEnum.ServerError),
+    ctx.delay(API_RESPONSE_DELAY),
   )
-}
+})
