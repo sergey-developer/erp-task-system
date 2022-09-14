4d26bb5b6f9b3a79c3af54254410bd97e86e95f5
sergey-developer <ijrtre123@gmail.com>
2022-09-14 11:42:32 +0300

    временные изменения.

--- craco.config.ts
+++ craco.config.ts
@@ -1,6 +1,11 @@
 import path from 'path'
 
+// import babelJsxPlugin from 'babel-plugin-jsx-remove-data-test-id'
+
+// const rewireBabelLoader = require('craco-babel-loader')
+const rewireBabelLoader = require('craco-babel-loader-plugin')
 const CracoAntDesignPlugin = require('craco-antd')
+// const babelJsxPlugin = require('babel-plugin-jsx-remove-data-test-id')
 
 const resolvePath = (p: string) => path.resolve(__dirname, p)
 
@@ -9,22 +14,34 @@ module.exports = {
     {
       plugin: CracoAntDesignPlugin,
       options: {
-        customizeThemeLessPath: resolvePath('./src/styles/customTheme.less'),
+        customizeThemeLessPath: resolvePath('src/styles/customTheme.less'),
+      },
+    },
+    {
+      plugin: rewireBabelLoader,
+      options: {
+        includes: [
+          resolvePath('node_modules/babel-plugin-jsx-remove-data-test-id'),
+        ],
       },
     },
   ],
+  // babel: {
+  //   presets: [],
+  //   plugins: [...whenProd(() => [new babelJsxPlugin()], [])],
+  // },
   webpack: {
     alias: {
-      lib: resolvePath('./src/lib'),
-      configs: resolvePath('./src/configs'),
-      modules: resolvePath('./src/modules'),
-      components: resolvePath('./src/components'),
-      state: resolvePath('./src/state'),
-      shared: resolvePath('./src/shared'),
-      styles: resolvePath('./src/styles'),
-      app: resolvePath('./src/app'),
-      _tests_: resolvePath('./src/_tests_'),
-      _fixtures_: resolvePath('./src/_fixtures_'),
+      lib: resolvePath('src/lib'),
+      configs: resolvePath('src/configs'),
+      modules: resolvePath('src/modules'),
+      components: resolvePath('src/components'),
+      state: resolvePath('src/state'),
+      shared: resolvePath('src/shared'),
+      styles: resolvePath('src/styles'),
+      app: resolvePath('src/app'),
+      _tests_: resolvePath('src/_tests_'),
+      _fixtures_: resolvePath('src/_fixtures_'),
     },
   },
 }
