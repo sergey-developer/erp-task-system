446dbfebd8013f32686eca8b75f8e9188b2c917a
sergey-developer <ijrtre123@gmail.com>
2022-09-16 15:13:11 +0300

    мелкие правки.

--- src/shared/services/api/api.service.ts
+++ src/shared/services/api/api.service.ts
@@ -1,12 +1,14 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 
+import { env } from 'configs/env'
+
 import baseQueryWithReauth from './baseQueryWithReauth'
 import { CACHE_TIME_LIFE } from './constants'
 
 const apiService = createApi({
   baseQuery: baseQueryWithReauth,
-  refetchOnFocus: true,
-  refetchOnReconnect: true,
+  refetchOnFocus: env.isProduction,
+  refetchOnReconnect: env.isProduction,
   keepUnusedDataFor: CACHE_TIME_LIFE,
   endpoints: () => ({}),
 })
