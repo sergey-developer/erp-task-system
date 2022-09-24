aa6e9a380322b2ec57a6a1d0d7d484dc205dcc9e
sergey-developer <ijrtre123@gmail.com>
2022-09-24 15:10:30 +0300

    поправил тип для пропсов

--- src/modules/task/features/TaskView/components/TaskDetails/AdditionalInfo/_tests_/constants.ts
+++ src/modules/task/features/TaskView/components/TaskDetails/AdditionalInfo/_tests_/constants.ts
@@ -2,21 +2,25 @@ import { Keys } from 'shared/interfaces/utils'
 
 import { AdditionalInfoProps } from '../index'
 
-export const baseProps: Pick<AdditionalInfoProps, 'expanded'> & {
-  onExpand: jest.MockedFn<AdditionalInfoProps['onExpand']>
-} = {
+export const baseProps: Readonly<
+  Pick<AdditionalInfoProps, 'expanded'> & {
+    onExpand: jest.MockedFn<AdditionalInfoProps['onExpand']>
+  }
+> = {
   expanded: false,
   onExpand: jest.fn(),
 }
 
-export const requiredProps: Pick<
-  AdditionalInfoProps,
-  | 'priority'
-  | 'severity'
-  | 'impact'
-  | 'productClassifier1'
-  | 'productClassifier2'
-  | 'productClassifier3'
+export const requiredProps: Readonly<
+  Pick<
+    AdditionalInfoProps,
+    | 'priority'
+    | 'severity'
+    | 'impact'
+    | 'productClassifier1'
+    | 'productClassifier2'
+    | 'productClassifier3'
+  >
 > = {
   severity: 'severity',
   priority: 'priority',
@@ -26,9 +30,8 @@ export const requiredProps: Pick<
   productClassifier3: 'productClassifier3',
 }
 
-export const notRequiredProps: Omit<
-  AdditionalInfoProps,
-  Keys<typeof baseProps> | Keys<typeof requiredProps>
+export const notRequiredProps: Readonly<
+  Omit<AdditionalInfoProps, Keys<typeof baseProps> | Keys<typeof requiredProps>>
 > = {
   email: 'email',
   sapId: 'sapId',
