9b166de13c024b0757b6f945304c67efd4f87011
sergey-developer <ijrtre123@gmail.com>
2022-10-05 14:16:11 +0300

    поправил типы.

--- src/modules/task/constants/dictionary.ts
+++ src/modules/task/constants/dictionary.ts
@@ -3,6 +3,7 @@ import {
   TaskExtraStatusEnum,
   TaskStatusEnum,
 } from 'modules/task/constants/common'
+import { BaseTaskModel } from 'modules/task/models'
 import { StringMap } from 'shared/interfaces/utils'
 
 import { FastFilterEnum } from '../features/TaskList/constants/common'
@@ -35,23 +36,25 @@ export const taskFilterStatusDict: Readonly<StringMap<FastFilterEnum.Overdue>> =
     [FastFilterEnum.Overdue]: 'Просроченные',
   }
 
-export const taskImpactMap = new Map([
-  [1, '1-всеохватывающее/широкое'],
-  [2, '2-значительное/большое'],
-  [3, '3-умеренное/ограниченное'],
-  [4, '4-малое/локализованное'],
-])
+export const taskImpactMap: Map<BaseTaskModel['initialImpact'], string> =
+  new Map([
+    [1, '1-всеохватывающее/широкое'],
+    [2, '2-значительное/большое'],
+    [3, '3-умеренное/ограниченное'],
+    [4, '4-малое/локализованное'],
+  ])
 
-export const taskSeverityMap = new Map([
+export const taskSeverityMap: Map<BaseTaskModel['severity'], string> = new Map([
   [1, '1-критическая'],
   [2, '2-высокая'],
   [3, '3-средняя'],
   [4, '4-низкая'],
 ])
 
-export const taskPriorityMap = new Map([
-  [1, '1-критический'],
-  [2, '2-высокий'],
-  [3, '3-средний'],
-  [4, '4-низкий'],
-])
+export const taskPriorityMap: Map<BaseTaskModel['priorityCode'], string> =
+  new Map([
+    [1, '1-критический'],
+    [2, '2-высокий'],
+    [3, '3-средний'],
+    [4, '4-низкий'],
+  ])
