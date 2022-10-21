c0f7a624c7977f634f9474b6c0f075926d410abd
sergey-developer <ijrtre123@gmail.com>
2022-10-21 18:15:29 +0300

    мелкие правки.

--- src/_fixtures_/task/taskComment.ts
+++ src/_fixtures_/task/taskComment.ts
@@ -3,12 +3,10 @@ import { generateDateString, generateId, generateWord } from '_tests_/utils'
 import { TaskCommentTypeEnum } from 'modules/task/constants/common'
 import { TaskDetailsCommentModel } from 'modules/task/features/TaskView/models'
 
-export const getTaskComment = (
-  data?: Partial<Pick<TaskDetailsCommentModel, 'text'>>,
-): TaskDetailsCommentModel => ({
+export const getTaskComment = (): TaskDetailsCommentModel => ({
   id: generateId(),
   taskId: generateId(),
-  text: data?.text || generateWord(),
+  text: generateWord(),
   createdAt: generateDateString(),
   type: TaskCommentTypeEnum.Common,
   author: getCommentAuthor(),
