f8409ab37866d475439109896e56a30a295b80aa
sergey-developer <ijrtre123@gmail.com>
2022-11-20 13:25:21 +0300

    мелкие правки.

--- src/modules/task/features/TaskView/components/TaskDetailsTabs/JournalTab/_tests_/JournalEntry.test.tsx
+++ src/modules/task/features/TaskView/components/TaskDetailsTabs/JournalTab/_tests_/JournalEntry.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { render } from '_tests_/utils'
+import { generateDateString, generateWord, render } from '_tests_/utils'
 import { screen } from '@testing-library/react'
 import {
   TaskJournalSourceEnum,
@@ -9,42 +9,25 @@ import {
 
 import JournalEntry, { JournalEntryProps } from '../JournalEntry'
 
-const journalEntry: JournalEntryProps = {
+const requiredProps: JournalEntryProps = {
   type: TaskJournalTypeEnum.StatusChange,
-  createdAt: new Date().toISOString(),
-  description: 'Выполнено переназначение',
+  createdAt: generateDateString(),
+  description: generateWord(),
   sourceSystem: TaskJournalSourceEnum.X5,
-  author: 'Александров Александр Александрович',
+  author: generateWord(),
 }
 
 describe('Элемент журнала', () => {
   test('Отображается корректно со всеми данными', () => {
-    render(
-      <JournalEntry
-        type={journalEntry.type}
-        author={journalEntry.author}
-        createdAt={journalEntry.createdAt}
-        description={journalEntry.description}
-        sourceSystem={journalEntry.sourceSystem}
-      />,
-    )
+    render(<JournalEntry {...requiredProps} />)
 
-    Object.values(journalEntry).forEach((item) => {
+    Object.values(requiredProps).forEach((item) => {
       expect(screen.getByText(item!)).toBeInTheDocument()
     })
   })
 
   test('Не отображает автора если не передать его', () => {
-    render(
-      <JournalEntry
-        author={null}
-        type={journalEntry.type}
-        createdAt={journalEntry.createdAt}
-        description={journalEntry.description}
-        sourceSystem={journalEntry.sourceSystem}
-      />,
-    )
-
+    render(<JournalEntry {...requiredProps} author={null} />)
     expect(screen.queryByTestId('journalEntry-author')).not.toBeInTheDocument()
   })
 })
