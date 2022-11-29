44fdc11640f87238168585484a7a9894c8140484
sergey-developer <ijrtre123@gmail.com>
2022-11-29 19:42:41 +0300

    написал тесты.

--- src/modules/task/features/TaskView/components/CreateSubTaskModal/_tests_/CreateSubTaskModal.test.tsx
+++ src/modules/task/features/TaskView/components/CreateSubTaskModal/_tests_/CreateSubTaskModal.test.tsx
@@ -1,58 +1,172 @@
-import { render } from '_tests_/utils'
+import { generateWord, render } from '_tests_/utils'
 
 import CreateSubTaskModal from '../index'
 import { requiredProps } from './constants'
+import testUtils from './utils'
 
 describe('Модалка создания задачи заявки', () => {
   test('Заголовок отображается', () => {
     render(<CreateSubTaskModal {...requiredProps} />)
+
+    expect(testUtils.getChildByText('Задание по заявке')).toBeInTheDocument()
+    expect(testUtils.getChildByText(requiredProps.recordId)).toBeInTheDocument()
   })
 
-  describe('Поле шаблона', () => {
-    test('Название поля отображается', () => {})
+  describe('Форма создания', () => {
+    describe('Поле шаблона', () => {
+      test('Название поля отображается', () => {
+        render(<CreateSubTaskModal {...requiredProps} />)
+        expect(testUtils.template.getLabel()).toBeInTheDocument()
+      })
 
-    test('Отображается корректно', () => {})
+      test('Отображается корректно', () => {
+        render(<CreateSubTaskModal {...requiredProps} />)
 
-    test('Закрыто по умолчанию', () => {})
+        const field = testUtils.template.getField()
 
-    test('Не активно во время загрузки', () => {})
+        expect(testUtils.template.getPlaceholder()).toBeInTheDocument()
+        expect(field).toBeInTheDocument()
+        expect(field).toBeEnabled()
+      })
 
-    test('Отображает состояние загрузки во время загрузки шаблонов', () => {})
+      test('Закрыто по умолчанию', () => {
+        render(<CreateSubTaskModal {...requiredProps} />)
+        expect(testUtils.template.queryField(true)).not.toBeInTheDocument()
+      })
 
-    test('Имеет верное количество вариантов', () => {})
+      test('Не активно во время загрузки', () => {
+        render(<CreateSubTaskModal {...requiredProps} isLoading />)
+        expect(testUtils.template.getField()).toBeDisabled()
+      })
 
-    test('Можно выбрать значение', () => {})
+      test('Отображает состояние загрузки во время загрузки шаблонов', () => {
+        render(
+          <CreateSubTaskModal {...requiredProps} templateOptionsIsLoading />,
+        )
+        testUtils.template.expectLoadingStarted()
+      })
 
-    test('Закрывается после выбора значения', () => {})
+      test('Имеет верное количество вариантов', () => {})
 
-    test('Можно установить значение по умолчанию', () => {})
-  })
+      test('Не имеет значения по умолчанию', () => {})
 
-  describe('Поле заголовка', () => {
-    test('Название поля отображается', () => {})
+      test('Можно выбрать значение', () => {})
 
-    test('Отображается корректно', () => {})
+      test('Закрывается после выбора значения', () => {})
 
-    test('Не активно во время загрузки', () => {})
+      test('Можно установить значение по умолчанию', () => {})
+    })
 
-    test('Можно ввести значение', () => {})
+    describe('Поле заголовка', () => {
+      test('Название поля отображается', () => {
+        render(<CreateSubTaskModal {...requiredProps} />)
+        expect(testUtils.title.getLabel()).toBeInTheDocument()
+      })
 
-    test('Можно очистить значение', () => {})
+      test('Отображается корректно', () => {
+        render(<CreateSubTaskModal {...requiredProps} />)
 
-    test('Можно установить значение по умолчанию', () => {})
-  })
+        const field = testUtils.title.getField()
+        expect(field).toBeInTheDocument()
+        expect(field).toBeEnabled()
+        expect(field).not.toHaveValue()
+      })
 
-  describe('Поле описания', () => {
-    test('Название поля отображается', () => {})
+      test('Не активно во время загрузки', () => {
+        render(<CreateSubTaskModal {...requiredProps} isLoading />)
+        expect(testUtils.title.getField()).toBeDisabled()
+      })
 
-    test('Отображается корректно', () => {})
+      test('Можно ввести значение', async () => {
+        const { user } = render(<CreateSubTaskModal {...requiredProps} />)
 
-    test('Не активно во время загрузки', () => {})
+        const value = generateWord()
+        const field = await testUtils.title.setValue(user, value)
 
-    test('Можно ввести значение', () => {})
+        expect(field).toHaveDisplayValue(value)
+      })
 
-    test('Можно очистить значение', () => {})
+      test('Можно очистить значение', async () => {
+        const { user } = render(<CreateSubTaskModal {...requiredProps} />)
 
-    test('Можно установить значение по умолчанию', () => {})
+        const value = generateWord()
+        await testUtils.title.setValue(user, value)
+        await testUtils.title.resetValue(user)
+
+        expect(testUtils.title.getField()).not.toHaveDisplayValue(value)
+      })
+
+      test('Можно установить значение по умолчанию', () => {
+        const initialValue = generateWord()
+
+        render(
+          <CreateSubTaskModal
+            {...requiredProps}
+            initialFormValues={{
+              title: initialValue,
+            }}
+          />,
+        )
+
+        expect(testUtils.title.getField()).toHaveDisplayValue(initialValue)
+      })
+    })
+
+    describe('Поле описания', () => {
+      test('Название поля отображается', () => {
+        render(<CreateSubTaskModal {...requiredProps} />)
+        expect(testUtils.description.getLabel()).toBeInTheDocument()
+      })
+
+      test('Отображается корректно', () => {
+        render(<CreateSubTaskModal {...requiredProps} />)
+
+        const field = testUtils.description.getField()
+        expect(field).toBeInTheDocument()
+        expect(field).toBeEnabled()
+        expect(field).not.toHaveValue()
+      })
+
+      test('Не активно во время загрузки', () => {
+        render(<CreateSubTaskModal {...requiredProps} isLoading />)
+        expect(testUtils.description.getField()).toBeDisabled()
+      })
+
+      test('Можно ввести значение', async () => {
+        const { user } = render(<CreateSubTaskModal {...requiredProps} />)
+
+        const value = generateWord()
+        const field = await testUtils.description.setValue(user, value)
+
+        expect(field).toHaveDisplayValue(value)
+      })
+
+      test('Можно очистить значение', async () => {
+        const { user } = render(<CreateSubTaskModal {...requiredProps} />)
+
+        const value = generateWord()
+        await testUtils.description.setValue(user, value)
+        await testUtils.description.resetValue(user)
+
+        expect(testUtils.description.getField()).not.toHaveDisplayValue(value)
+      })
+
+      test('Можно установить значение по умолчанию', () => {
+        const initialValue = generateWord()
+
+        render(
+          <CreateSubTaskModal
+            {...requiredProps}
+            initialFormValues={{
+              description: initialValue,
+            }}
+          />,
+        )
+
+        expect(testUtils.description.getField()).toHaveDisplayValue(
+          initialValue,
+        )
+      })
+    })
   })
 })
--- src/modules/task/features/TaskView/components/CreateSubTaskModal/_tests_/utils.ts
+++ src/modules/task/features/TaskView/components/CreateSubTaskModal/_tests_/utils.ts
@@ -1,11 +1,103 @@
+import {
+  getButtonIn,
+  getSelect,
+  loadingStartedBySelect,
+  querySelect,
+} from '_tests_/utils'
 import { screen, within } from '@testing-library/react'
+import { UserEvent } from '@testing-library/user-event/setup/setup'
 
 const getContainer = () => screen.getByRole('dialog')
 const getChildByText = (text: string) => within(getContainer()).getByText(text)
 
+// template field
+const getTemplateFieldContainer = () =>
+  within(getContainer()).getByTestId('template')
+
+const getTemplateField = (opened?: boolean) =>
+  getSelect(getTemplateFieldContainer(), { expanded: opened })
+
+const queryTemplateField = (opened?: boolean) =>
+  querySelect(getTemplateFieldContainer(), { expanded: opened })
+
+const getTemplateFieldPlaceholder = () =>
+  within(getTemplateFieldContainer()).getByText('Наименование шаблона')
+
+const getTemplateFieldLabel = () =>
+  within(getTemplateFieldContainer()).getByTitle('Шаблон')
+
+const templateFieldExpectLoadingStarted = async () => {
+  await loadingStartedBySelect(getTemplateFieldContainer())
+}
+
+// title field
+const getTitleFieldContainer = () => within(getContainer()).getByTestId('title')
+
+const getTitleField = () =>
+  within(getTitleFieldContainer()).getByPlaceholderText(
+    'Опишите коротко задачу',
+  )
+
+const getTitleFieldLabel = () =>
+  within(getTitleFieldContainer()).getByTitle('Краткое описание')
+
+const userSetTitle = async (user: UserEvent, value: string) => {
+  const field = getTitleField()
+  await user.type(field, value)
+  return field
+}
+
+const userResetTitle = async (user: UserEvent) => {
+  const button = getButtonIn(getTitleFieldContainer(), 'close-circle')
+  await user.click(button)
+}
+
+// description field
+const getDescriptionFieldContainer = () =>
+  within(getContainer()).getByTestId('description')
+
+const getDescriptionField = () =>
+  within(getDescriptionFieldContainer()).getByPlaceholderText(
+    'Расскажите подробнее о задаче',
+  )
+
+const getDescriptionFieldLabel = () =>
+  within(getDescriptionFieldContainer()).getByTitle('Подробное описание')
+
+const userSetDescription = async (user: UserEvent, value: string) => {
+  const field = getTitleField()
+  await user.type(field, value)
+  return field
+}
+
+const userResetDescription = async (user: UserEvent) => {
+  const button = getButtonIn(getDescriptionFieldContainer(), 'close-circle')
+  await user.click(button)
+}
+
 const utils = {
   getContainer,
   getChildByText,
+
+  template: {
+    getField: getTemplateField,
+    queryField: queryTemplateField,
+    getPlaceholder: getTemplateFieldPlaceholder,
+    getLabel: getTemplateFieldLabel,
+    expectLoadingStarted: templateFieldExpectLoadingStarted,
+  },
+  title: {
+    getField: getTitleField,
+    getLabel: getTitleFieldLabel,
+    setValue: userSetTitle,
+    resetValue: userResetTitle,
+  },
+  description: {
+    getField: getDescriptionField,
+    getLabel: getDescriptionFieldLabel,
+    setValue: userSetDescription,
+    resetValue: userResetDescription,
+  },
 }
 
 export default utils
