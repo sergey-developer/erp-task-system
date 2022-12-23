0c84e4c5be968e0cbb1a167881babd0a39fa7232
sergey-developer <ijrtre123@gmail.com>
2022-12-23 11:18:02 +0300

    добавил тесты

---
+++ src/_tests_/mocks/api/subTask/cancelSubTask.mock.ts
@@ -0,0 +1,29 @@
+import {
+  getBadRequestErrorMockFn,
+  getRequestMockFn,
+  getServerErrorMockFn,
+  getSuccessMockFn,
+} from '_tests_/mocks/request'
+import { ResponseResolverOptions } from '_tests_/mocks/response'
+import { ReworkSubTaskResponseModel } from 'modules/subTask/models'
+import { getCancelSubTaskUrl } from 'modules/subTask/utils/apiUrls'
+import { HttpMethodEnum } from 'shared/constants/http'
+import { ErrorData } from 'shared/services/api'
+
+const getCancelSubTaskMockFn = (subTaskId: number) =>
+  getRequestMockFn(HttpMethodEnum.Delete, getCancelSubTaskUrl(subTaskId))
+
+export const mockCancelSubTaskSuccess = (
+  subTaskId: number,
+  options?: Partial<ResponseResolverOptions<ReworkSubTaskResponseModel>>,
+) => getSuccessMockFn(getCancelSubTaskMockFn(subTaskId), options)()
+
+export const mockCancelSubTaskBadRequestError = <T extends object>(
+  subTaskId: number,
+  options?: Partial<ResponseResolverOptions<ErrorData<T>>>,
+) => getBadRequestErrorMockFn(getCancelSubTaskMockFn(subTaskId), options)()
+
+export const mockCancelSubTaskServerError = <T extends object>(
+  subTaskId: number,
+  options?: Partial<ResponseResolverOptions<ErrorData<T>>>,
+) => getServerErrorMockFn(getCancelSubTaskMockFn(subTaskId), options)()
