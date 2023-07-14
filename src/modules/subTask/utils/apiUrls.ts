9009dd6889e78b5356767b0b8a8eb56db17725c7
sergey-developer <ijrtre123@gmail.com>
2023-07-14 17:44:29 +0300

    убрал применение функции appendSlashAtEnd

--- src/modules/subTask/utils/apiUrls.ts
+++ src/modules/subTask/utils/apiUrls.ts
@@ -2,27 +2,17 @@ import { generatePath } from 'react-router-dom'
 
 import { SubTaskEndpointEnum } from 'modules/subTask/constants/api'
 
-import { appendSlashAtEnd } from 'shared/utils/string'
-
 export const getSubTaskListUrl = (taskId: number): string =>
-  appendSlashAtEnd(
-    generatePath(SubTaskEndpointEnum.GetSubTaskList, { id: String(taskId) }),
-  )
+  generatePath(SubTaskEndpointEnum.GetSubTaskList, { id: String(taskId) })
 
 export const getSubTaskTemplateListUrl = (): string =>
   SubTaskEndpointEnum.GetSubTaskTemplateList
 
 export const createSubTaskUrl = (taskId: number): string =>
-  appendSlashAtEnd(
-    generatePath(SubTaskEndpointEnum.CreateSubTask, { id: String(taskId) }),
-  )
+  generatePath(SubTaskEndpointEnum.CreateSubTask, { id: String(taskId) })
 
 export const cancelSubTaskUrl = (subTaskId: number): string =>
-  appendSlashAtEnd(
-    generatePath(SubTaskEndpointEnum.CancelSubTask, { id: String(subTaskId) }),
-  )
+  generatePath(SubTaskEndpointEnum.CancelSubTask, { id: String(subTaskId) })
 
 export const reworkSubTaskUrl = (subTaskId: number): string =>
-  appendSlashAtEnd(
-    generatePath(SubTaskEndpointEnum.ReworkSubTask, { id: String(subTaskId) }),
-  )
+  generatePath(SubTaskEndpointEnum.ReworkSubTask, { id: String(subTaskId) })
--- src/modules/user/utils/apiUrls.ts
+++ src/modules/user/utils/apiUrls.ts
@@ -2,14 +2,8 @@ import { generatePath } from 'react-router-dom'
 
 import { UserEndpointEnum } from 'modules/user/constants/api'
 
-import { appendSlashAtEnd } from 'shared/utils/string'
-
 export const updateUserUrl = (userId: number): string =>
-  appendSlashAtEnd(
-    generatePath(UserEndpointEnum.UpdateUser, { id: String(userId) }),
-  )
+  generatePath(UserEndpointEnum.UpdateUser, { id: String(userId) })
 
 export const updateUserStatusUrl = (userId: number): string =>
-  appendSlashAtEnd(
-    generatePath(UserEndpointEnum.UpdateUserStatus, { id: String(userId) }),
-  )
+  generatePath(UserEndpointEnum.UpdateUserStatus, { id: String(userId) })
