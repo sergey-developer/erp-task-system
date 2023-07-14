70b179852513ba0678120cc75395261199ca1678
sergey-developer <ijrtre123@gmail.com>
2023-07-14 18:16:35 +0300

    страница склада

---
+++ src/modules/warehouse/pages/WarehousePage/WarehousePage.test.tsx
@@ -0,0 +1,95 @@
+import { screen, within } from '@testing-library/react'
+
+import { render } from '_tests_/utils'
+
+import WarehousePage from './index'
+
+const getContainer = () => screen.getByTestId('warehouse-page')
+
+const getChildByText = (value: string) =>
+  within(getContainer()).getByText(value)
+
+const testUtils = {
+  getContainer,
+  getChildByText,
+}
+
+describe('Страница склада', () => {
+  describe('Наименование объекта', () => {
+    test('Отображается корректно', () => {
+      render(<WarehousePage />)
+
+      const label = testUtils.getChildByText('Наименование объекта')
+      const value = testUtils.getChildByText('ООО “СКЛАД”')
+
+      expect(label).toBeInTheDocument()
+      expect(value).toBeInTheDocument()
+    })
+  })
+
+  describe('Родительский склад', () => {
+    test('Отображается корректно', () => {
+      render(<WarehousePage />)
+
+      const label = testUtils.getChildByText('Родительский склад')
+      const value = testUtils.getChildByText('ООО “СКЛАД-ОСНОВНОЙ”')
+
+      expect(label).toBeInTheDocument()
+      expect(value).toBeInTheDocument()
+    })
+  })
+
+  describe('Юридическое лицо', () => {
+    test('Отображается корректно', () => {
+      render(<WarehousePage />)
+
+      const label = testUtils.getChildByText('Юридическое лицо')
+      const value = testUtils.getChildByText(
+        'ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ "СКЛАД"',
+      )
+
+      expect(label).toBeInTheDocument()
+      expect(value).toBeInTheDocument()
+    })
+  })
+
+  describe('Адрес', () => {
+    test('Отображается корректно', () => {
+      render(<WarehousePage />)
+
+      const label = testUtils.getChildByText('Адрес')
+      const value = testUtils.getChildByText(
+        '123112, город Москва, Пресненская наб, д. 10 стр. 2, помещ. 5н офис 337',
+      )
+
+      expect(label).toBeInTheDocument()
+      expect(value).toBeInTheDocument()
+    })
+  })
+
+  describe('Договор', () => {
+    test('Отображается корректно', () => {
+      render(<WarehousePage />)
+
+      const label = testUtils.getChildByText('Договор')
+      const value = testUtils.getChildByText(
+        'Договор складского хранения №55369',
+      )
+
+      expect(label).toBeInTheDocument()
+      expect(value).toBeInTheDocument()
+    })
+  })
+
+  describe('Прочие данные', () => {
+    test('Отображается корректно', () => {
+      render(<WarehousePage />)
+
+      const label = testUtils.getChildByText('Прочие данные')
+      const value = testUtils.getChildByText('Прочие данные о складе')
+
+      expect(label).toBeInTheDocument()
+      expect(value).toBeInTheDocument()
+    })
+  })
+})
--- src/modules/warehouse/pages/WarehousePage/index.tsx
+++ src/modules/warehouse/pages/WarehousePage/index.tsx
@@ -1,7 +1,37 @@
-import { FC } from "react";
+import { FC } from 'react'
+
+import LabeledData from 'components/LabeledData'
+import Space from 'components/Space'
+
+import { WrapperStyled } from './styles'
 
 const WarehousePage: FC = () => {
-  return <div>WarehousePage</div>
+  return (
+    <WrapperStyled data-testid='warehouse-page'>
+      <Space $block direction='vertical'>
+        <LabeledData label='Наименование объекта'>ООО “СКЛАД”</LabeledData>
+
+        <LabeledData label='Родительский склад'>
+          ООО “СКЛАД-ОСНОВНОЙ”
+        </LabeledData>
+
+        <LabeledData label='Юридическое лицо'>
+          ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ "СКЛАД"
+        </LabeledData>
+
+        <LabeledData label='Адрес'>
+          123112, город Москва, Пресненская наб, д. 10 стр. 2, помещ. 5н офис
+          337
+        </LabeledData>
+
+        <LabeledData label='Договор'>
+          Договор складского хранения №55369
+        </LabeledData>
+
+        <LabeledData label='Прочие данные'>Прочие данные о складе</LabeledData>
+      </Space>
+    </WrapperStyled>
+  )
 }
 
 export default WarehousePage
