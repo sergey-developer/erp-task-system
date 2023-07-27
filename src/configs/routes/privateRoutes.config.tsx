11f6a20d3771696e49cc6eb4fce81b5191458249
sergey-developer <ijrtre123@gmail.com>
2023-07-27 16:58:46 +0300

    авто правки yarn lint-staged

--- src/configs/routes/privateRoutes.config.tsx
+++ src/configs/routes/privateRoutes.config.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Navigate, RouteObject } from 'react-router-dom'
 
+import { routes as staffRoutes } from 'modules/monitoring/routes'
+import { routes as taskRoutes } from 'modules/task/routes'
 import { UserModel } from 'modules/user/models'
 
 import PrivateLayout from 'components/Layouts/PrivateLayout'
 import NotFoundPage from 'components/Pages/NotFoundPage'
 
 import { RouteEnum } from './constants'
-import { routes as staffRoutes } from 'modules/monitoring/routes'
-import { routes as taskRoutes } from 'modules/task/routes'
 
 const ChangePasswordPage = React.lazy(
   () => import('modules/auth/pages/ChangePasswordPage'),
--- src/modules/task/features/TaskListMap/styles.ts
+++ src/modules/task/features/TaskListMap/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const MapWrapperStyled = styled.div`
   width: 100%;
