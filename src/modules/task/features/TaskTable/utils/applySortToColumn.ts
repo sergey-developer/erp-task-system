5178f993b8b4ba64f16415ffc33fa1c1b5258799
sergey-developer <ijrtre123@gmail.com>
2023-08-01 16:05:04 +0300

    правки импорта isEqual

--- src/modules/task/features/TaskTable/utils/applySortToColumn.ts
+++ src/modules/task/features/TaskTable/utils/applySortToColumn.ts
@@ -1,16 +1,14 @@
-import { ColumnsType } from 'antd/es/table'
+import { ColumnType } from 'antd/es/table'
 import isEqual from 'lodash/isEqual'
-
-import { ArrayFirst } from 'shared/interfaces/utils'
 
 import { SortValue } from '../constants/sort'
 import { TaskTableListItem } from '../interfaces'
 import { parseSort } from './parseSort'
 
 export const applySortToColumn = (
-  column: ArrayFirst<ColumnsType<TaskTableListItem>>,
+  column: ColumnType<TaskTableListItem>,
   sort: SortValue,
-): ArrayFirst<ColumnsType<TaskTableListItem>> => {
+): ColumnType<TaskTableListItem> => {
   const sorterResult = parseSort(sort)
 
   if (!isEqual(column.key, sorterResult.columnKey)) return column
