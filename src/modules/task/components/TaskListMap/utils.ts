f76c3b7247aa832009c4cf6d69de8bb2253bd1bb
sergey-developer <ijrtre123@gmail.com>
2023-08-04 11:02:37 +0300

    Если все заявки в кластере с одинаковыми координатами, то отображается не кластер, а маркер с цветом который соответствует приоритетному типу заявки и при нажатии отправляется запрос.
    Приближение при нажатии на кластер вместо отправки запроса если заявки не с одинаковыми координатами.

--- src/modules/task/components/TaskListMap/utils.ts
+++ src/modules/task/components/TaskListMap/utils.ts
@@ -1,7 +1,14 @@
+import isEqual from 'lodash/isEqual'
+import { Feature } from 'ol'
+import { FeatureLike } from 'ol/Feature'
+import OlMap from 'ol/Map'
 import { Coordinate } from 'ol/coordinate'
+import { Extent } from 'ol/extent'
 import { Circle, Fill, Style, Text } from 'ol/style'
 
 import { TaskTypeEnum } from 'modules/task/constants'
+
+import { isTruthy } from 'shared/utils/common'
 
 import {
   incidentMarkerColor,
@@ -12,6 +19,7 @@ import {
   selectedIncidentTaskOrRequestTaskMarkerStyle,
   selectedRequestMarkerStyle,
 } from './styles'
+import { FeatureData, Geometry } from './types'
 
 export const formatCoords = (coords: Coordinate, maxFractionDigits = 8) =>
   coords.map((c) =>
@@ -21,6 +29,60 @@ export const formatCoords = (coords: Coordinate, maxFractionDigits = 8) =>
       }),
     ),
   )
+
+export const getFeaturesGeometry = (
+  features: Feature[],
+): (Geometry & { getCoordinates: () => Coordinate })[] =>
+  features
+    .map(
+      (f) => f.getGeometry() as Geometry & { getCoordinates: () => Coordinate },
+    )
+    .filter(isTruthy)
+
+export const getFeaturesCoordinate = (geometries: Geometry[]): Coordinate[] =>
+  geometries.map((g) => g.getCoordinates())
+
+export const getGeometriesExtent = (geometries: Geometry[]): Extent[] =>
+  geometries.map((g) => g.getExtent())
+
+export const fitMapToExtent = (map: OlMap, extent: Extent): void => {
+  if (extent.length) {
+    map.getView().fit(extent, { duration: 1000, padding: [100, 100, 100, 100] })
+  }
+}
+
+export const checkFeaturesHaveSameCoords = (coords: Coordinate[]): boolean =>
+  coords.every((coords, index, array) => array.every((c) => isEqual(c, coords)))
+
+export const getFeaturesData = (features: Feature[]): FeatureData[] =>
+  features.map((f) => f.get('data'))
+
+export const getFeatureData = (feature: Feature): FeatureData =>
+  feature.get('data')
+
+export const getFeaturesWithin = (feature: Feature | FeatureLike): Feature[] =>
+  feature.get('features')
+
+export const getPriorityTaskType = (
+  featuresData: FeatureData[],
+): FeatureData['type'] => {
+  let type: TaskTypeEnum
+
+  const incident = featuresData.find(
+    ({ type }) => type === TaskTypeEnum.Incident,
+  )
+  const request = featuresData.find(({ type }) => type === TaskTypeEnum.Request)
+
+  if (incident) {
+    type = incident.type
+  } else if (request) {
+    type = request.type
+  } else {
+    type = TaskTypeEnum.IncidentTask
+  }
+
+  return type
+}
 
 export const getClusterStyle = (size: number): Style =>
   new Style({
@@ -61,5 +123,3 @@ export const getSelectedMarkerStyle = (type: TaskTypeEnum): Style => {
       return selectedIncidentTaskOrRequestTaskMarkerStyle
   }
 }
-
-export const styleCache: Partial<Record<number | TaskTypeEnum, Style>> = {}
