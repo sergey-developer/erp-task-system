682285616baef85b8cfacf978e42cab163ee2817
sergey-developer <ijrtre123@gmail.com>
2023-08-15 13:18:39 +0300

    тесты отображения пункта номенклатуры

--- src/modules/warehouse/pages/WarehouseCatalogListPage/index.tsx
+++ src/modules/warehouse/pages/WarehouseCatalogListPage/index.tsx
@@ -3,18 +3,49 @@ import { Link } from 'react-router-dom'
 
 import { RouteEnum } from 'configs/routes'
 
+import MatchUserPermissions from 'modules/user/components/MatchUserPermissions'
+import { UserPermissions } from 'modules/user/models'
+
 import CatalogListItem from './CatalogListItem'
+
+type CatalogItem = {
+  link: RouteEnum
+  text: string
+  permissions?: UserPermissions[]
+}
+
+const catalogList: CatalogItem[] = [
+  {
+    link: RouteEnum.WarehouseList,
+    text: 'Склады',
+  },
+  {
+    link: RouteEnum.NomenclatureList,
+    text: 'Номенклатура',
+    permissions: ['NOMENCLATURES_READ'],
+  },
+]
 
 const WarehouseCatalogListPage: FC = () => {
   return (
     <div data-testid='warehouse-catalog-list-page'>
-      <CatalogListItem>
-        <Link to={RouteEnum.WarehouseList}>Склады</Link>
-      </CatalogListItem>
-
-      <CatalogListItem>
-        <Link to={RouteEnum.NomenclatureList}>Номенклатура</Link>
-      </CatalogListItem>
+      {catalogList.map(({ link, text, permissions }, index) =>
+        !!permissions?.length ? (
+          <MatchUserPermissions key={index} expected={permissions}>
+            {({ permissions }) =>
+              Object.values(permissions).every(Boolean) ? (
+                <CatalogListItem>
+                  <Link to={link}>{text}</Link>
+                </CatalogListItem>
+              ) : null
+            }
+          </MatchUserPermissions>
+        ) : (
+          <CatalogListItem key={index}>
+            <Link to={link}>{text}</Link>
+          </CatalogListItem>
+        ),
+      )}
     </div>
   )
 }
