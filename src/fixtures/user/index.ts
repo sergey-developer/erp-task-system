fb739b80332b92629cb2a3fdb9dccda11d2706fd
sergey-developer <ijrtre123@gmail.com>
2023-08-16 17:25:49 +0300

    фикстуры и моки

--- src/fixtures/user/index.ts
+++ src/fixtures/user/index.ts
@@ -1,9 +1,11 @@
 import * as user from './user'
+import * as userList from './userStatusList'
 import * as userStatus from './userStatus'
 import * as userStatusList from './userStatusList'
 
 const userFixtures = {
   ...user,
+  ...userList,
   ...userStatus,
   ...userStatusList,
 } as const
