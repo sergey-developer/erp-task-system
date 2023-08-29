a13d59d7cef5b87055855f5cac2e492d3225052c
sergey-developer <ijrtre123@gmail.com>
2023-08-29 20:00:28 +0300

    правки стилей

--- src/modules/warehouse/components/Equipment/index.tsx
+++ src/modules/warehouse/components/Equipment/index.tsx
@@ -26,124 +26,134 @@ const Equipment: FC<EquipmentProps> = ({
         {equipment && (
           <Space $block direction='vertical' size='middle'>
             <Row data-testid='title'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Наименование:</Text>
               </Col>
 
-              <Col span={12}>{equipment.title}</Col>
+              <Col span={16}>{equipment.title}</Col>
             </Row>
 
             <Row data-testid='category'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Категория:</Text>
               </Col>
 
-              <Col span={12}>{equipment.category.title}</Col>
+              <Col span={16}>{equipment.category.title}</Col>
             </Row>
 
             <Row data-testid='nomenclature'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Номенклатура:</Text>
               </Col>
 
-              <Col span={12}>{equipment.nomenclature.title}</Col>
+              <Col span={16}>{equipment.nomenclature.title}</Col>
             </Row>
 
             {displayableFields.includes('customerInventoryNumber') && (
               <Row data-testid='customer-inventory-number'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>Инвентарный номер заказчика:</Text>
                 </Col>
 
-                <Col span={12}>{valueOrHyphen(equipment.customerInventoryNumber)}</Col>
+                <Col span={16}>{valueOrHyphen(equipment.customerInventoryNumber)}</Col>
               </Row>
             )}
 
             {displayableFields.includes('inventoryNumber') && (
               <Row data-testid='inventory-number'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>Инвентарный номер:</Text>
                 </Col>
 
-                <Col span={12}>{valueOrHyphen(equipment.inventoryNumber)}</Col>
+                <Col span={16}>{valueOrHyphen(equipment.inventoryNumber)}</Col>
               </Row>
             )}
 
             {equipment.nomenclature.equipmentHasSerialNumber && (
               <Row data-testid='serial-number'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>Серийный номер:</Text>
                 </Col>
 
-                <Col span={12}>{valueOrHyphen(equipment.serialNumber)}</Col>
+                <Col span={16}>{valueOrHyphen(equipment.serialNumber)}</Col>
               </Row>
             )}
 
             <Row data-testid='warehouse'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Склад:</Text>
               </Col>
 
-              <Col span={12}>{valueOrHyphen(equipment.warehouse?.title)}</Col>
+              <Col span={16}>{valueOrHyphen(equipment.warehouse?.title)}</Col>
             </Row>
 
             <Row data-testid='condition'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Состояние:</Text>
               </Col>
 
-              <Col span={12}>{equipmentConditionDict[equipment.condition]}</Col>
+              <Col span={16}>{equipmentConditionDict[equipment.condition]}</Col>
             </Row>
 
             <Row data-testid='created-at'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Дата оприходования:</Text>
               </Col>
 
-              <Col span={12}>{formatDate(equipment.createdAt, DATE_FORMAT)}</Col>
+              <Col span={16}>{formatDate(equipment.createdAt, DATE_FORMAT)}</Col>
             </Row>
 
             <Row data-testid='created-by'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Кем оприходовано:</Text>
               </Col>
 
-              <Col span={12}>{equipment.createdBy.fullName}</Col>
+              <Col span={16}>{equipment.createdBy.fullName}</Col>
             </Row>
 
             <Row data-testid='quantity'>
-              <Col span={7}>
+              <Col span={8}>
                 <Text type='secondary'>Количество:</Text>
               </Col>
 
-              <Col span={17}>
+              <Col span={16}>
                 <Row gutter={16}>
                   <Col span={6}>{valueOrHyphen(equipment.quantity)}</Col>
 
                   <Col span={18}>
-                    <Space>
-                      <Text type='secondary'>Ед. измерения:</Text>
-                      <Text>{equipment.measurementUnit.title}</Text>
-                    </Space>
+                    <Row>
+                      <Col span={6}>
+                        <Text type='secondary'>Ед. изм:</Text>
+                      </Col>
+
+                      <Col span={18}>
+                        <Text>{equipment.measurementUnit.title}</Text>
+                      </Col>
+                    </Row>
                   </Col>
                 </Row>
               </Col>
             </Row>
 
             <Row data-testid='price'>
-              <Col span={7}>
+              <Col span={8}>
                 <Text type='secondary'>Стоимость:</Text>
               </Col>
 
-              <Col span={17}>
+              <Col span={16}>
                 <Row gutter={16}>
                   <Col span={6}>{valueOrHyphen(equipment.price)}</Col>
 
                   <Col span={18}>
-                    <Space>
-                      <Text type='secondary'>Валюта:</Text>
-                      <Text>{valueOrHyphen(equipment.currency?.title)}</Text>
-                    </Space>
+                    <Row>
+                      <Col span={6}>
+                        <Text type='secondary'>Валюта:</Text>
+                      </Col>
+
+                      <Col span={18}>
+                        <Text>{valueOrHyphen(equipment.currency?.title)}</Text>
+                      </Col>
+                    </Row>
                   </Col>
                 </Row>
               </Col>
@@ -151,68 +161,68 @@ const Equipment: FC<EquipmentProps> = ({
 
             {displayableFields.includes('isNew') && (
               <Row data-testid='is-new'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>Новое:</Text>
                 </Col>
 
-                <Col span={12}>{getYesNo(equipment.isNew)}</Col>
+                <Col span={16}>{getYesNo(equipment.isNew)}</Col>
               </Row>
             )}
 
             {displayableFields.includes('isWarranty') && (
               <Row data-testid='is-warranty'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>На гарантии:</Text>
                 </Col>
 
-                <Col span={12}>{getYesNo(equipment.isWarranty)}</Col>
+                <Col span={16}>{getYesNo(equipment.isWarranty)}</Col>
               </Row>
             )}
 
             {displayableFields.includes('isRepaired') && (
               <Row data-testid='is-repaired'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>Отремонтированное:</Text>
                 </Col>
 
-                <Col span={12}>{getYesNo(equipment.isRepaired)}</Col>
+                <Col span={16}>{getYesNo(equipment.isRepaired)}</Col>
               </Row>
             )}
 
             {displayableFields.includes('usageCounter') && (
               <Row data-testid='usage-counter'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>Счётчик пробега текущий:</Text>
                 </Col>
 
-                <Col span={12}>{valueOrHyphen(equipment.usageCounter)}</Col>
+                <Col span={16}>{valueOrHyphen(equipment.usageCounter)}</Col>
               </Row>
             )}
 
             {displayableFields.includes('owner') && (
               <Row data-testid='owner'>
-                <Col span={12}>
+                <Col span={8}>
                   <Text type='secondary'>Владелец оборудования:</Text>
                 </Col>
 
-                <Col span={12}>{valueOrHyphen(equipment.owner?.title)}</Col>
+                <Col span={16}>{valueOrHyphen(equipment.owner?.title)}</Col>
               </Row>
             )}
 
             <Row data-testid='purpose'>
-              <Col span={7}>
+              <Col span={8}>
                 <Text type='secondary'>Назначение оборудования:</Text>
               </Col>
 
-              <Col span={17}>{equipment.purpose.title}</Col>
+              <Col span={16}>{equipment.purpose.title}</Col>
             </Row>
 
             <Row data-testid='comment'>
-              <Col span={12}>
+              <Col span={8}>
                 <Text type='secondary'>Комментарий:</Text>
               </Col>
 
-              <Col span={12}>{valueOrHyphen(equipment.comment)}</Col>
+              <Col span={16}>{valueOrHyphen(equipment.comment)}</Col>
             </Row>
           </Space>
         )}
