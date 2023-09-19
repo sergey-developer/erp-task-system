270037a1e4895c52ad2ace0e1afe6e5daf539cd5
sergey-developer <ijrtre123@gmail.com>
2023-09-19 14:01:55 +0300

    добавил параметр к запросу

--- src/modules/warehouse/models/nomenclature/getNomenclatureList.model.ts
+++ src/modules/warehouse/models/nomenclature/getNomenclatureList.model.ts
@@ -1,14 +1,15 @@
+import { NomenclatureListItemModel } from 'modules/warehouse/models'
+
 import { PaginatedListSuccessResponse } from 'shared/models/pagination.model'
 import { PaginationParams } from 'shared/types/pagination'
 import { MaybeUndefined } from 'shared/types/utils'
-
-import { NomenclatureListItemModel } from './nomenclatureList.model'
 
 export type GetNomenclatureListQueryArgs = MaybeUndefined<
   Partial<
     PaginationParams & {
       search: string
       group: number
+      equipmentHasSerialNumber: boolean
     }
   >
 >
