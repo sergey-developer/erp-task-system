e8ebfb03375cd4e7372a02cedfe496a8ebb37d75
sergey-developer <ijrtre123@gmail.com>
2023-09-21 10:53:52 +0300

    правки модели

--- src/modules/task/models/suspendRequest.model.ts
+++ src/modules/task/models/suspendRequest.model.ts
@@ -1,10 +1,12 @@
 import { SuspendRequestStatusEnum } from 'modules/task/constants'
 import { BaseUserModel } from 'modules/user/models'
 
+import { MaybeNull } from 'shared/types/utils'
+
 export type SuspendRequestModel = {
   id: number
   status: SuspendRequestStatusEnum
   comment: string
-  author: Omit<BaseUserModel, 'avatar'>
+  author: MaybeNull<Omit<BaseUserModel, 'avatar'>>
   suspendEndAt: string
 }
