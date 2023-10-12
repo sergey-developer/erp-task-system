2b0d039fd5f7e41f101823eab25bca9a88fa06ee
sergey-developer <ijrtre123@gmail.com>
2023-10-12 22:21:24 +0300

    правки отображения валюты при присмотре и редактировании заявки на перемещение

--- src/modules/warehouse/models/relocationTask/relocationEquipmentList.model.ts
+++ src/modules/warehouse/models/relocationTask/relocationEquipmentList.model.ts
@@ -1,5 +1,6 @@
 import { EquipmentConditionEnum } from 'modules/warehouse/constants/equipment'
 
+import { CurrencyModel } from 'shared/models/currency'
 import { IdType } from 'shared/types/common'
 import { MaybeNull } from 'shared/types/utils'
 
@@ -11,7 +12,7 @@ export type RelocationEquipmentListItemModel = {
   quantity: number
 
   price: MaybeNull<number>
-  currency: MaybeNull<number>
+  currency: MaybeNull<CurrencyModel>
   serialNumber: MaybeNull<string>
 }
 
