0aaed3c6dd6f4ac089a50b5e0dc5011704c377bb
sergey-developer <ijrtre123@gmail.com>
2023-10-17 15:49:16 +0300

    пункт меню, тесты

--- src/modules/warehouse/hooks/relocationTask/index.ts
+++ src/modules/warehouse/hooks/relocationTask/index.ts
@@ -1,5 +1,6 @@
 export * from './useGetRelocationTask'
 export * from './useLazyGetRelocationTaskWaybillM15'
+export * from './useRelocationTaskStatus'
 
 export * from './useGetRelocationTaskList'
 
