2dd3629c937c4ac0ddf65fc9bfd07de762b21dea
sergey-developer <ijrtre123@gmail.com>
2023-10-17 12:48:46 +0300

    добавлена кнопка копирования

--- src/modules/task/components/CardTabs/DescriptionTab/index.tsx
+++ src/modules/task/components/CardTabs/DescriptionTab/index.tsx
@@ -8,27 +8,22 @@ import Space from 'components/Space'
 
 import { renderStringWithLineBreak } from 'shared/utils/string'
 
-const { Title, Paragraph } = Typography
+import { TitleStyled } from './style'
 
-export type DescriptionTabProps = Pick<
-  TaskModel,
-  'description' | 'attachments'
-> & {
+const { Paragraph } = Typography
+
+export type DescriptionTabProps = Pick<TaskModel, 'description' | 'attachments'> & {
   title: string
 }
 
-const DescriptionTab: FC<DescriptionTabProps> = ({
-  title,
-  description,
-  attachments = [],
-}) => {
+const DescriptionTab: FC<DescriptionTabProps> = ({ title, description, attachments = [] }) => {
   return (
     <Space data-testid='task-description-tab' $block direction='vertical'>
-      <Title level={5}>{title}</Title>
+      <TitleStyled level={5} copyable={{ text: `${title}\n\n${description}` }}>
+        {title}
+      </TitleStyled>
 
-      {description && (
-        <Paragraph>{renderStringWithLineBreak(description)}</Paragraph>
-      )}
+      {description && <Paragraph>{renderStringWithLineBreak(description)}</Paragraph>}
 
       {!!attachments?.length && <AttachmentList attachments={attachments} />}
     </Space>
