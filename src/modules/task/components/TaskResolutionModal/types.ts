53218489b3ac47d251e68da36d8f907019a0c022
s.sergeev <s.sergeev@rsvn.ru>
2023-11-10 14:25:02 +0300

    удалил лишний файл

--- src/modules/task/components/TaskResolutionModal/types.ts
+++
@@ -1,7 +0,0 @@
-import { UploadFile } from 'antd/es/upload/interface'
-
-export type TaskResolutionFormFields = {
-  techResolution: string
-  userResolution?: string
-  attachments?: UploadFile[]
-}
