1c3ef90a883b600496080edc0ecf1e832399d83a
s.sergeev <s.sergeev@rsvn.ru>
2023-12-07 19:21:08 +0600

    автокомплит для полей исполнитель, объект выбытия, объект прибытия.

--- src/shared/utils/common/selectFilter.ts
+++ src/shared/utils/common/selectFilter.ts
@@ -4,7 +4,10 @@ import isString from 'lodash/isString'
 // todo: переиспользовать в других местах
 export const filterOptionBy =
   (optionKey: string): SelectProps<any, any>['filterOption'] =>
-  (input, option) =>
-    option && isString(option[optionKey])
+  (input, option) => {
+    if (!option || option.options) return false
+
+    return isString(option[optionKey])
       ? option[optionKey].toLowerCase().includes(input.toLowerCase())
       : false
+  }
