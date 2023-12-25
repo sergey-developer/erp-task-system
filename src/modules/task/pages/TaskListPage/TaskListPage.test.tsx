fd3e3dec0b92cb0dc591a4856142697a33038ded
s.sergeev <s.sergeev@rsvn.ru>
2023-12-25 11:19:00 +0600

    тест счётчиков при переводе на 2ю линию.

--- src/modules/task/pages/TaskListPage/TaskListPage.test.tsx
+++ src/modules/task/pages/TaskListPage/TaskListPage.test.tsx
@@ -15,6 +15,12 @@ import {
   activeExecuteTaskItemProps,
   testUtils as cardTitleTestUtils,
 } from 'modules/task/components/TaskDetails/Title/Title.test'
+import {
+  activeSecondLineButtonProps,
+  showSecondLineButtonProps,
+  testUtils as workGroupBlockTestUtils,
+} from 'modules/task/components/TaskDetails/WorkGroupBlock/WorkGroupBlock.test'
+import { testUtils as taskSecondLineModalTestUtils } from 'modules/task/components/TaskSecondLineModal/TaskSecondLineModal.test'
 import { testUtils as taskTableTestUtils } from 'modules/task/components/TaskTable/TaskTable.test'
 import { testUtils as tasksFiltersStorageTestUtils } from 'modules/task/components/TasksFiltersStorage/TasksFiltersStorage.test'
 import { testUtils as updateTasksButtonTestUtils } from 'modules/task/components/UpdateTasksButton/UpdateTasksButton.test'
@@ -43,6 +49,7 @@ import {
   mockGetUserListSuccess,
   mockGetWorkGroupListSuccess,
   mockResolveTaskSuccess,
+  mockUpdateTaskWorkGroupSuccess,
 } from '_tests_/mocks/api'
 import {
   buttonTestUtils,
@@ -428,6 +435,48 @@ describe('Страница реестра заявок', () => {
       await executeTaskModalTestUtils.setTechResolution(user, fakeWord())
       await executeTaskModalTestUtils.clickSubmitButton(user)
       await executeTaskModalTestUtils.expectLoadingFinished()
+      await fastFilterListTestUtils.expectLoadingStarted()
+      await fastFilterListTestUtils.expectLoadingFinished()
+    })
+
+    test('Перезапрашивается при переводе на 2-ю линию', async () => {
+      mockGetTaskCountersSuccess({ once: false })
+
+      const taskListItem = taskFixtures.taskListItem()
+      mockGetTaskListSuccess({
+        body: commonFixtures.paginatedListResponse([taskListItem]),
+        once: false,
+      })
+
+      const task = taskFixtures.task({
+        id: taskListItem.id,
+        workGroup: showSecondLineButtonProps.workGroup,
+        status: activeSecondLineButtonProps.status,
+        extendedStatus: activeSecondLineButtonProps.extendedStatus,
+      })
+      mockGetTaskSuccess(task.id, { body: task })
+
+      const workGroup = workGroupFixtures.workGroupListItem()
+      mockGetWorkGroupListSuccess({ body: [workGroup], once: false })
+
+      mockUpdateTaskWorkGroupSuccess(task.id)
+
+      const { user } = render(<TaskListPage />, { store: getStoreWithAuth() })
+
+      await taskTableTestUtils.expectLoadingFinished()
+      await fastFilterListTestUtils.expectLoadingFinished()
+      await taskTableTestUtils.clickRow(user, task.id)
+      await taskDetailsTestUtils.findContainer()
+      await taskDetailsTestUtils.expectTaskLoadingFinished()
+
+      await workGroupBlockTestUtils.clickSecondLineButton(user)
+      await taskSecondLineModalTestUtils.findContainer()
+      await taskSecondLineModalTestUtils.expectWorkGroupLoadingFinished()
+      await taskSecondLineModalTestUtils.openWorkGroupField(user)
+      await taskSecondLineModalTestUtils.selectWorkGroup(user, workGroup.name)
+      await taskSecondLineModalTestUtils.setComment(user, fakeWord())
+      await taskSecondLineModalTestUtils.clickSubmitButton(user)
+
       await fastFilterListTestUtils.expectLoadingStarted()
       await fastFilterListTestUtils.expectLoadingFinished()
     })
