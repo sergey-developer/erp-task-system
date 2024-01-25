d882b1173b8573df7de6ef51fafac22b79c2c72c
s.sergeev <s.sergeev@rsvn.ru>
2024-01-25 23:43:24 +0600

    просмотр общих фото

--- src/modules/attachment/components/AttachmentList/index.tsx
+++ src/modules/attachment/components/AttachmentList/index.tsx
@@ -1,19 +1,9 @@
 import { Image, Space, Typography } from 'antd'
 import { FC } from 'react'
 
-import { AttachmentListModel } from 'modules/attachment/models'
-import { RelocationTaskAttachmentsModel } from 'modules/warehouse/models'
-
-import { hasProperty } from 'shared/utils/common'
+import { AttachmentListProps } from './types'
 
 const { Text } = Typography
-
-export type AttachmentListProps = {
-  data: AttachmentListModel | RelocationTaskAttachmentsModel
-
-  imgWidth?: number
-  imgHeight?: number
-}
 
 const AttachmentList: FC<AttachmentListProps> = ({
   data,
@@ -27,12 +17,8 @@ const AttachmentList: FC<AttachmentListProps> = ({
         data.map((item, index) => (
           <Image
             key={index}
-            src={
-              // todo: попросить унифицировать контракт
-              hasProperty(item, 'thumbnail')
-                ? item.thumbnail || ''
-                : item.thumbnails.mediumThumbnail
-            }
+            alt={item.name}
+            src={item.thumbnails?.mediumThumbnail}
             preview={{ src: item.url, movable: false }}
             width={imgWidth}
             height={imgHeight}
