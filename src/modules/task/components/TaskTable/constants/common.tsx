e109c6af0c34240dd1ef488389f20d801670345e
s.sergeev <s.sergeev@rsvn.ru>
2024-01-30 15:43:39 +0600

    Адаптация таблицы реестра под зум

--- src/modules/task/components/TaskTable/constants/common.tsx
+++ src/modules/task/components/TaskTable/constants/common.tsx
@@ -1,10 +1,5 @@
-import { TableProps } from 'antd'
 import { TableLocale } from 'antd/es/table/interface'
-
-import { TaskTableListItem } from '../types'
 
 export const localeConfig: Pick<TableLocale, 'emptyText'> = {
   emptyText: 'По заданным параметрам фильтрации ни одна заявка не найдена',
 }
-
-export const scrollConfig: TableProps<TaskTableListItem>['scroll'] = { y: 600 }
