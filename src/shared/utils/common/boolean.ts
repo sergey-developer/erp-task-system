0859d4994a2eef939f41f958a3f930683852dfcc
s.sergeev <s.sergeev@rsvn.ru>
2024-02-02 15:59:22 +0600

    правки после конфликтов

--- src/shared/utils/common/boolean.ts
+++ src/shared/utils/common/boolean.ts
@@ -1,6 +1,6 @@
-import { Truthy } from 'shared/types/utils'
+import { IsTruthyType } from 'shared/types/utils'
 
 export const isTrue = (value: any): boolean => value === true
-export const isTruthy = <T>(value: T): value is Truthy<T> => Boolean(value)
+export const isTruthy = <T>(value: T): value is IsTruthyType<T> => Boolean(value)
 
 export const isFalse = (value: any): boolean => value === false
--- src/shared/utils/common/isTruthy.ts
+++
@@ -1,3 +0,0 @@
-import { IsTruthyType } from 'shared/types/utils'
-
-export const isTruthy = <T>(value: T): value is IsTruthyType<T> => Boolean(value)
