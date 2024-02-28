4dafb92d48df1cada7f73463df084368cd41b260
s.sergeev <s.sergeev@rsvn.ru>
2024-02-28 11:56:28 +0600

    правки решения конфликтов

--- src/modules/warehouse/constants/relocationTask/selectOptions.ts
+++ src/modules/warehouse/constants/relocationTask/selectOptions.ts
@@ -1,7 +1,7 @@
 import { SelectProps } from 'antd'
 import { DefaultOptionType } from 'rc-select/lib/Select'
 
-import { MatchExpectedPermissionsResult } from 'modules/user/utils'
+import { MatchedPermissions } from 'modules/user/utils'
 
 import {
   externalRelocationStatusDict,
@@ -19,7 +19,7 @@ export const relocationTaskStatusOptions: SelectProps['options'] = Object.keys(
 
 // relocation task type options
 export type RelocationTaskTypeOption = DefaultOptionType & {
-  hasPermissions?: (permissions: MatchExpectedPermissionsResult) => boolean
+  hasPermissions?: (permissions: MatchedPermissions) => boolean
 }
 
 export const relocationTaskTypeOptions: RelocationTaskTypeOption[] = [
