c4fbc5c8003152401d208c4b286fc6564c0ce4ed
s.sergeev <s.sergeev@rsvn.ru>
2024-03-01 11:58:11 +0600

    тесты

--- src/modules/warehouse/pages/CreateDocumentsPackagePage/index.tsx
+++ src/modules/warehouse/pages/CreateDocumentsPackagePage/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 
 const CreateDocumentsPackagePage: FC = () => {
-  return <div>CreateDocumentsPackagePage</div>
+  return <div data-testid='create-documents-package-page'>CreateDocumentsPackagePage</div>
 }
 
 export default CreateDocumentsPackagePage
