7c23c0bd906e4a7fd11686429f040e88c204000a
s.sergeev <s.sergeev@rsvn.ru>
2024-03-13 16:49:44 +0600

    правки после конфликтов

--- src/modules/task/models/taskCompletionDocument/createTaskCompletionDocuments.model.ts
+++ src/modules/task/models/taskCompletionDocument/createTaskCompletionDocuments.model.ts
@@ -1,6 +1,6 @@
-import { BaseTaskRequestArgs } from 'modules/task/types'
+import { TaskRequestArgs } from 'modules/task/types'
 
 import { Base64Type } from 'shared/types/common'
 
-export type CreateTaskCompletionDocumentsMutationArgs = BaseTaskRequestArgs
+export type CreateTaskCompletionDocumentsMutationArgs = TaskRequestArgs
 export type CreateTaskCompletionDocumentsSuccessResponse = Base64Type
