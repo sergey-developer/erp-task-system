231a2e1a8baff0a1428c800ac35d075fdd514778
s.sergeev <s.sergeev@rsvn.ru>
2024-04-19 14:20:33 +0600

    правки отображения номенклатуры

--- src/modules/warehouse/constants/inventorization/api.ts
+++ src/modules/warehouse/constants/inventorization/api.ts
@@ -2,3 +2,7 @@ export enum InventorizationApiEnum {
   GetInventorizations = '/inventorizations/',
   CreateInventorization = '/inventorizations/',
 }
+
+export enum InventorizationApiTagEnum {
+  Inventorizations = 'Inventorizations',
+}
