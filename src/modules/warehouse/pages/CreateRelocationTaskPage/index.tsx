b4050909c986371452f0f44395974486216b00e1
s.sergeev <s.sergeev@rsvn.ru>
2024-06-10 09:48:49 +0300

    правки тестов CreateRelocationTaskPage

--- src/modules/warehouse/pages/CreateRelocationTaskPage/index.tsx
+++ src/modules/warehouse/pages/CreateRelocationTaskPage/index.tsx
@@ -785,6 +785,7 @@ const CreateRelocationTaskPage: FC = () => {
                   <Col>
                     <Space>
                       <Upload
+                        data-testid='add-from-excel-upload'
                         showUploadList={false}
                         beforeUpload={stubFalse}
                         fileList={[]}
