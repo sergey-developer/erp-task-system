14b53149c76f69c154bae78d9c770412c79eaa04
s.sergeev <s.sergeev@rsvn.ru>
2024-06-19 13:47:25 +0300

    правки цвета полей у таблицы с нередактируемыми данными

--- src/modules/warehouse/components/ReviseEquipmentTable/styles.ts
+++ src/modules/warehouse/components/ReviseEquipmentTable/styles.ts
@@ -4,4 +4,10 @@ export const TableWrapperStyled = styled.div`
   && .ant-select-selector {
     border-color: inherit;
   }
+
+  .ant-input-disabled,
+  .ant-input-disabled::placeholder,
+  .ant-input-number-disabled {
+    color: inherit;
+  }
 `
