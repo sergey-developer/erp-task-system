589ed6b6f6668ba9e822f5cda2fe758247c4ac04
s.sergeev <s.sergeev@rsvn.ru>
2024-07-09 18:04:25 +0300

    кнопка добавления оборудования, получение данных для модалки

--- src/modules/warehouse/components/ExecuteInventorizationReviseTab/index.tsx
+++ src/modules/warehouse/components/ExecuteInventorizationReviseTab/index.tsx
@@ -1,15 +1,21 @@
-import { useSetState } from 'ahooks'
-import { Col, Flex, Input, Row, Typography } from 'antd'
+import { useBoolean, useSetState } from 'ahooks'
+import { Button, Col, Flex, Input, Row, Typography } from 'antd'
 import { SearchProps } from 'antd/es/input'
 import isNumber from 'lodash/isNumber'
-import React, { FC, useCallback, useState } from 'react'
+import React, { FC, useCallback, useMemo, useState } from 'react'
 
+import { EquipmentConditionEnum } from 'modules/warehouse/constants/equipment'
+import {
+  useGetEquipmentCatalogList,
+  useGetEquipmentCategories,
+} from 'modules/warehouse/hooks/equipment'
 import {
   useGetInventorizationEquipments,
   useUpdateInventorizationEquipment,
 } from 'modules/warehouse/hooks/inventorization'
 import { GetInventorizationEquipmentsQueryArgs } from 'modules/warehouse/models'
 import { InventorizationRequestArgs } from 'modules/warehouse/types'
+import { checkEquipmentCategoryIsConsumable } from 'modules/warehouse/utils/equipment'
 
 import { undefinedSelectOption } from 'shared/constants/selectField'
 import { useGetLocations } from 'shared/hooks/catalogs/location'
@@ -36,6 +42,41 @@ const ExecuteInventorizationReviseTab: FC<ExecuteInventorizationReviseTabProps> 
   inventorizationId,
 }) => {
   const [searchValue, setSearchValue] = useState<string>()
+
+  const [
+    createEquipmentModalOpened,
+    { setTrue: openCreateEquipmentModal, setFalse: closeCreateEquipmentModal },
+  ] = useBoolean(false)
+
+  const debouncedOpenCreateEquipmentModal = useDebounceFn(openCreateEquipmentModal)
+  const debouncedCloseCreateEquipmentModal = useDebounceFn(closeCreateEquipmentModal)
+
+  const {
+    currentData: equipmentCategories = [],
+    isFetching: equipmentCategoriesIsFetching,
+    isSuccess: isEquipmentCategoriesFetchedSuccess,
+  } = useGetEquipmentCategories(undefined, { skip: !createEquipmentModalOpened })
+
+  const notConsumableEquipmentCategoriesIds = useMemo(
+    () =>
+      equipmentCategories
+        .filter((c) => !checkEquipmentCategoryIsConsumable(c.code))
+        .map((c) => c.id),
+    [equipmentCategories],
+  )
+
+  const { currentData: equipmentCatalogs = [], isFetching: equipmentCatalogsIsFetching } =
+    useGetEquipmentCatalogList(
+      {
+        conditions: [
+          EquipmentConditionEnum.Working,
+          EquipmentConditionEnum.Broken,
+          EquipmentConditionEnum.NonRepairable,
+        ],
+        categories: notConsumableEquipmentCategoriesIds,
+      },
+      { skip: !createEquipmentModalOpened || !isEquipmentCategoriesFetchedSuccess },
+    )
 
   const { currentData: locations = [], isFetching: locationsIsFetching } = useGetLocations({
     responsibilityArea: false,
@@ -108,33 +149,39 @@ const ExecuteInventorizationReviseTab: FC<ExecuteInventorizationReviseTabProps> 
     setSearchValue(event.target.value)
 
   return (
-    <Flex data-testid='execute-inventorization-revise-tab' vertical gap='small'>
-      <Title level={5}>Перечень оборудования для сверки</Title>
+    <>
+      <Flex data-testid='execute-inventorization-revise-tab' vertical gap='small'>
+        <Title level={5}>Перечень оборудования для сверки</Title>
 
-      <Row>
-        <Col span={5}>
-          <Search
-            allowClear
-            onSearch={onSearch}
-            onChange={onChangeSearch}
-            value={searchValue}
-            placeholder='Поиск оборудования'
-            disabled={inventorizationEquipmentsIsFetching}
-          />
-        </Col>
-      </Row>
+        <Row>
+          <Col span={5}>
+            <Search
+              allowClear
+              onSearch={onSearch}
+              onChange={onChangeSearch}
+              value={searchValue}
+              placeholder='Поиск оборудования'
+              disabled={inventorizationEquipmentsIsFetching}
+            />
+          </Col>
 
-      <ReviseEquipmentTable
-        pagination={extractPaginationParams(paginatedInventorizationEquipments)}
-        dataSource={extractPaginationResults(paginatedInventorizationEquipments)}
-        loading={inventorizationEquipmentsIsFetching}
-        locations={locations}
-        locationsIsLoading={locationsIsFetching}
-        onTableChange={onChangeTable}
-        onChangeQuantityFact={onChangeQuantityFact}
-        onChangeLocationFact={onChangeLocationFact}
-      />
-    </Flex>
+          <Col>
+            <Button onClick={debouncedOpenCreateEquipmentModal}>Добавить оборудование</Button>
+          </Col>
+        </Row>
+
+        <ReviseEquipmentTable
+          pagination={extractPaginationParams(paginatedInventorizationEquipments)}
+          dataSource={extractPaginationResults(paginatedInventorizationEquipments)}
+          loading={inventorizationEquipmentsIsFetching}
+          locations={locations}
+          locationsIsLoading={locationsIsFetching}
+          onTableChange={onChangeTable}
+          onChangeQuantityFact={onChangeQuantityFact}
+          onChangeLocationFact={onChangeLocationFact}
+        />
+      </Flex>
+    </>
   )
 }
 
