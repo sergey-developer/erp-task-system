32d2cde134445b2263bb49af32133d5ac90d27a6
Александр Меньшиков <a.menshikov@rsvn.ru>
2024-09-20 22:14:37 +0300

    Вынес testUtils для ConfirmExecutionRelocationTaskModal

--- src/_tests_/features/warehouse/ConfirmExecutionRelocationTaskModal/constants.ts
+++ src/_tests_/features/warehouse/ConfirmExecutionRelocationTaskModal/constants.ts
@@ -1,1 +1,12 @@
-export enum TestIdsEnum {}
+import { ConfirmExecutionRelocationTaskModalProps } from 'modules/warehouse/components/ConfirmExecutionRelocationTaskModal/index'
+
+export const props: ConfirmExecutionRelocationTaskModalProps = {
+  open: true,
+  isLoading: false,
+  onCancel: jest.fn(),
+  onConfirm: jest.fn(),
+}
+
+export enum TestIdsEnum {
+  ConfirmExecutionRelocationTaskModal = 'confirm-execution-relocation-task-modal',
+}
--- src/modules/warehouse/components/ConfirmExecutionRelocationTaskModal/ConfirmExecutionRelocationTaskModal.test.tsx
+++ src/modules/warehouse/components/ConfirmExecutionRelocationTaskModal/ConfirmExecutionRelocationTaskModal.test.tsx
@@ -1,55 +1,20 @@
-import { screen, within } from '@testing-library/react'
-import { UserEvent } from '@testing-library/user-event/setup/setup'
+import { within } from '@testing-library/react'
 
-import { buttonTestUtils, render } from '_tests_/utils'
+import { props } from '_tests_/features/warehouse/ConfirmExecutionRelocationTaskModal/constants'
+import { confirmExecutionRelocationTaskModalTestUtils } from '_tests_/features/warehouse/ConfirmExecutionRelocationTaskModal/testUtils'
+import { render } from '_tests_/utils'
 
-import ConfirmExecutionRelocationTaskModal, { ConfirmExecutionRelocationTaskModalProps } from './index'
-
-const props: ConfirmExecutionRelocationTaskModalProps = {
-  open: true,
-  isLoading: false,
-  onCancel: jest.fn(),
-  onConfirm: jest.fn(),
-}
-
-const getContainer = () => screen.getByTestId('confirm-execution-relocation-task-modal')
-const findContainer = () => screen.findByTestId('confirm-execution-relocation-task-modal')
-
-// confirm button
-const getConfirmButton = () => buttonTestUtils.getButtonIn(getContainer(), /Подтвердить выполнение/)
-const clickConfirmButton = async (user: UserEvent) => {
-  const button = getConfirmButton()
-  await user.click(button)
-}
-
-// cancel button
-const getCancelButton = () => buttonTestUtils.getButtonIn(getContainer(), 'Отменить')
-const clickCancelButton = async (user: UserEvent) => {
-  const button = getCancelButton()
-  await user.click(button)
-}
-
-export const testUtils = {
-  getContainer,
-  findContainer,
-
-  getConfirmButton,
-  clickConfirmButton,
-
-  getCancelButton,
-  clickCancelButton,
-
-  expectLoadingStarted: () => buttonTestUtils.expectLoadingStarted(getConfirmButton()),
-  expectLoadingFinished: () => buttonTestUtils.expectLoadingFinished(getConfirmButton()),
-}
+import ConfirmExecutionRelocationTaskModal from './index'
 
 describe('Модалка отмены заявки на перемещение', () => {
   test('Модалка отображается корректно', () => {
     render(<ConfirmExecutionRelocationTaskModal {...props} />)
 
-    const container = getContainer()
+    const container = confirmExecutionRelocationTaskModalTestUtils.getContainer()
     const title = within(container).getByText('Подтверждение выполнения')
-    const description = within(container).getByText('Вы уверены, что хотите подтвердить выполнение заявки?')
+    const description = within(container).getByText(
+      'Вы уверены, что хотите подтвердить выполнение заявки?',
+    )
 
     expect(title).toBeInTheDocument()
     expect(description).toBeInTheDocument()
@@ -59,7 +24,7 @@ describe('Модалка отмены заявки на перемещение',
     test('Отображается корректно', () => {
       render(<ConfirmExecutionRelocationTaskModal {...props} />)
 
-      const button = testUtils.getConfirmButton()
+      const button = confirmExecutionRelocationTaskModalTestUtils.getConfirmButton()
 
       expect(button).toBeInTheDocument()
       expect(button).toBeEnabled()
@@ -67,7 +32,7 @@ describe('Модалка отмены заявки на перемещение',
 
     test('При клике обработчик вызывается', async () => {
       const { user } = render(<ConfirmExecutionRelocationTaskModal {...props} />)
-      await testUtils.clickConfirmButton(user)
+      await confirmExecutionRelocationTaskModalTestUtils.clickConfirmButton(user)
       expect(props.onConfirm).toBeCalledTimes(1)
     })
   })
@@ -76,7 +41,7 @@ describe('Модалка отмены заявки на перемещение',
     test('Отображается корректно', () => {
       render(<ConfirmExecutionRelocationTaskModal {...props} />)
 
-      const button = testUtils.getCancelButton()
+      const button = confirmExecutionRelocationTaskModalTestUtils.getCancelButton()
 
       expect(button).toBeInTheDocument()
       expect(button).toBeEnabled()
@@ -84,7 +49,7 @@ describe('Модалка отмены заявки на перемещение',
 
     test('При клике обработчик вызывается', async () => {
       const { user } = render(<ConfirmExecutionRelocationTaskModal {...props} />)
-      await testUtils.clickCancelButton(user)
+      await confirmExecutionRelocationTaskModalTestUtils.clickCancelButton(user)
       expect(props.onCancel).toBeCalledTimes(1)
     })
   })
