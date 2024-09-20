daa45bf9a27368622b63ca422b6741e493459b69
Александр Меньшиков <a.menshikov@rsvn.ru>
2024-09-20 22:21:25 +0300

    Вынес testUtils для CreateEquipmentTechnicalExaminationModal

--- src/_tests_/features/warehouse/CreateEquipmentTechnicalExaminationModal/constants.ts
+++ src/_tests_/features/warehouse/CreateEquipmentTechnicalExaminationModal/constants.ts
@@ -1,1 +1,16 @@
-export enum TestIdsEnum {}
+import { CreateEquipmentTechnicalExaminationModalProps } from 'modules/warehouse/components/CreateEquipmentTechnicalExaminationModal/types'
+
+export const props: CreateEquipmentTechnicalExaminationModalProps = {
+  open: true,
+  isLoading: false,
+  onCancel: jest.fn(),
+  onSubmit: jest.fn(),
+}
+
+export enum TestIdsEnum {
+  CreateEquipmentTechnicalExaminationModal = 'create-equipment-technical-examination-modal',
+  MalfunctionFormItem = 'malfunction-form-item',
+  ConclusionFormItem = 'conclusion-form-item',
+  RestorationActionFormItem = 'restoration-action-form-item',
+  RestorationCostFormItem = 'restoration-cost-form-item',
+}
