37ad69954410796dc06382357ae9829209710136
Александр Меньшиков <a.menshikov@rsvn.ru>
2024-09-24 19:56:57 +0300

    Вынес testUtils для WarehouseListFilter

--- src/_tests_/features/warehouse/WarehouseListFilter/constants.ts
+++ src/_tests_/features/warehouse/WarehouseListFilter/constants.ts
@@ -1,1 +1,18 @@
-export enum TestIdsEnum {}
+import { WarehouseListFilterProps } from 'modules/warehouse/components/WarehouseListFilter/types'
+
+export const props: Readonly<WarehouseListFilterProps> = {
+  visible: true,
+  formValues: {},
+  onApply: jest.fn(),
+  onClose: jest.fn(),
+}
+
+export enum TestIdsEnum {
+  WarehouseListFilter = 'warehouse-list-filter',
+  TitleFilter = 'title-filter',
+  LegalEntityFilter = 'legal-entity-filter',
+  LegalEntitySelect = 'legal-entity-select',
+  AddressFilter = 'address-filter',
+  ParentFilter = 'parent-filter',
+  ParentSelect = 'parent-select',
+}
--- src/modules/warehouse/components/WarehouseListFilter/WarehouseListFilter.test.tsx
+++ src/modules/warehouse/components/WarehouseListFilter/WarehouseListFilter.test.tsx
@@ -1,168 +1,10 @@
-import { screen, within } from '@testing-library/react'
-import { UserEvent } from '@testing-library/user-event/setup/setup'
-
+import { props } from '_tests_/features/warehouse/WarehouseListFilter/constants'
+import { warehouseListFilterTestUtils } from '_tests_/features/warehouse/WarehouseListFilter/testUtils'
 import warehouseFixtures from '_tests_/fixtures/warehouse'
 import { mockGetLegalEntityListSuccess, mockGetWarehouseListSuccess } from '_tests_/mocks/api'
-import { fakeWord, selectTestUtils, render, setupApiTests, buttonTestUtils } from '_tests_/utils'
+import { fakeWord, render, setupApiTests } from '_tests_/utils'
 
 import WarehouseListFilter from './index'
-import { WarehouseListFilterProps } from './types'
-
-const props: Readonly<WarehouseListFilterProps> = {
-  visible: true,
-  formValues: {},
-  onApply: jest.fn(),
-  onClose: jest.fn(),
-}
-
-const getContainer = () => screen.getByTestId('warehouse-list-filter')
-
-const findContainer = (): Promise<HTMLElement> => screen.findByTestId('warehouse-list-filter')
-
-// reset button
-const getResetAllButton = () => buttonTestUtils.getButtonIn(getContainer(), /Сбросить все/)
-
-const clickResetButtonIn = async (user: UserEvent, container: HTMLElement) => {
-  const button = buttonTestUtils.getButtonIn(container, /сбросить/i)
-  await user.click(button)
-}
-
-const clickResetAllButton = async (user: UserEvent) => {
-  const button = getResetAllButton()
-  await user.click(button)
-}
-
-// close button
-const getCloseButton = () => buttonTestUtils.getButtonIn(getContainer(), /close/i)
-
-const clickCloseButton = async (user: UserEvent) => {
-  const button = getCloseButton()
-  await user.click(button)
-}
-
-// apply button
-const getApplyButton = () => buttonTestUtils.getButtonIn(getContainer(), /Применить/)
-
-const clickApplyButton = async (user: UserEvent) => {
-  const button = getApplyButton()
-  await user.click(button)
-}
-
-// title
-const getTitleFilter = () => within(getContainer()).getByTestId('title-filter')
-
-const getTitleFilterLabel = () => within(getTitleFilter()).getByText('Наименование объекта')
-
-const getTitleInput = () => within(getTitleFilter()).getByPlaceholderText('Ключевое слово')
-
-const setTitle = async (user: UserEvent, value: string) => {
-  const input = getTitleInput()
-  await user.type(input, value)
-  return input
-}
-
-const resetTitle = (user: UserEvent) => clickResetButtonIn(user, getTitleFilter())
-
-// legal entity
-const getLegalEntityFilter = () => within(getContainer()).getByTestId('legal-entity-filter')
-
-const getLegalEntityFilterLabel = () => within(getLegalEntityFilter()).getByText('Юридическое лицо')
-
-const getLegalEntitySelect = () => within(getLegalEntityFilter()).getByTestId('legal-entity-select')
-
-const openLegalEntitySelect = (user: UserEvent) =>
-  selectTestUtils.openSelect(user, getLegalEntitySelect())
-
-const setLegalEntity = selectTestUtils.clickSelectOption
-
-const getSelectedLegalEntity = () => selectTestUtils.getSelectedOption(getLegalEntitySelect())
-
-const resetLegalEntity = (user: UserEvent) => clickResetButtonIn(user, getLegalEntityFilter())
-
-const expectLegalEntityLoadingFinished = async () => {
-  const select = getLegalEntitySelect()
-  await selectTestUtils.expectLoadingFinished(select)
-}
-
-// address
-const getAddressFilter = () => within(getContainer()).getByTestId('address-filter')
-
-const getAddressFilterLabel = () => within(getAddressFilter()).getByText('Адрес')
-
-const getAddressInput = () => within(getAddressFilter()).getByPlaceholderText('Ключевое слово')
-
-const setAddress = async (user: UserEvent, value: string) => {
-  const input = getAddressInput()
-  await user.type(input, value)
-  return input
-}
-
-const resetAddress = (user: UserEvent) => clickResetButtonIn(user, getAddressFilter())
-
-// parent
-const getParentFilter = () => within(getContainer()).getByTestId('parent-filter')
-
-const getParentFilterLabel = () => within(getParentFilter()).getByText('Родительский склад')
-
-const getParentSelect = () => within(getParentFilter()).getByTestId('parent-select')
-
-const openParentSelect = (user: UserEvent) => selectTestUtils.openSelect(user, getParentSelect())
-
-const setParent = selectTestUtils.clickSelectOption
-
-const getSelectedParent = () => selectTestUtils.getSelectedOption(getParentSelect())
-
-const resetParent = (user: UserEvent) => clickResetButtonIn(user, getParentFilter())
-
-const expectParentLoadingFinished = async () => {
-  const select = getLegalEntitySelect()
-  await selectTestUtils.expectLoadingFinished(select)
-}
-
-export const testUtils = {
-  getContainer,
-  findContainer,
-
-  getResetAllButton,
-  clickResetButtonIn,
-  clickResetAllButton,
-
-  getCloseButton,
-  clickCloseButton,
-
-  getApplyButton,
-  clickApplyButton,
-
-  getTitleFilter,
-  getTitleFilterLabel,
-  getTitleInput,
-  setTitle,
-  resetTitle,
-
-  getLegalEntityFilter,
-  getLegalEntityFilterLabel,
-  getLegalEntitySelect,
-  openLegalEntitySelect,
-  setLegalEntity,
-  getSelectedLegalEntity,
-  resetLegalEntity,
-  expectLegalEntityLoadingFinished,
-
-  getAddressFilter,
-  getAddressFilterLabel,
-  getAddressInput,
-  setAddress,
-  resetAddress,
-
-  getParentFilter,
-  getParentFilterLabel,
-  getParentSelect,
-  openParentSelect,
-  setParent,
-  getSelectedParent,
-  resetParent,
-  expectParentLoadingFinished,
-}
 
 setupApiTests()
 
@@ -174,7 +16,7 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} />)
 
-      const button = testUtils.getCloseButton()
+      const button = warehouseListFilterTestUtils.getCloseButton()
 
       expect(button).toBeInTheDocument()
       expect(button).toBeEnabled()
@@ -186,7 +28,7 @@ describe('Фильтр списка складов', () => {
 
       const { user } = render(<WarehouseListFilter {...props} />)
 
-      await testUtils.clickCloseButton(user)
+      await warehouseListFilterTestUtils.clickCloseButton(user)
       expect(props.onClose).toBeCalledTimes(1)
     })
   })
@@ -198,7 +40,7 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} />)
 
-      const button = testUtils.getApplyButton()
+      const button = warehouseListFilterTestUtils.getApplyButton()
 
       expect(button).toBeInTheDocument()
       expect(button).toBeEnabled()
@@ -210,7 +52,7 @@ describe('Фильтр списка складов', () => {
 
       const { user } = render(<WarehouseListFilter {...props} />)
 
-      await testUtils.clickApplyButton(user)
+      await warehouseListFilterTestUtils.clickApplyButton(user)
 
       expect(props.onApply).toBeCalledTimes(1)
       expect(props.onApply).toBeCalledWith(expect.anything())
@@ -224,7 +66,7 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} />)
 
-      const button = testUtils.getResetAllButton()
+      const button = warehouseListFilterTestUtils.getResetAllButton()
 
       expect(button).toBeInTheDocument()
       expect(button).toBeEnabled()
@@ -252,12 +94,12 @@ describe('Фильтр списка складов', () => {
         />,
       )
 
-      await testUtils.clickResetAllButton(user)
+      await warehouseListFilterTestUtils.clickResetAllButton(user)
 
-      expect(testUtils.getTitleInput()).not.toHaveDisplayValue(titleValue)
-      expect(testUtils.getAddressInput()).not.toHaveDisplayValue(addressValue)
-      expect(testUtils.getSelectedParent()).not.toBeInTheDocument()
-      expect(testUtils.getSelectedLegalEntity()).not.toBeInTheDocument()
+      expect(warehouseListFilterTestUtils.getTitleInput()).not.toHaveDisplayValue(titleValue)
+      expect(warehouseListFilterTestUtils.getAddressInput()).not.toHaveDisplayValue(addressValue)
+      expect(warehouseListFilterTestUtils.getSelectedParent()).not.toBeInTheDocument()
+      expect(warehouseListFilterTestUtils.getSelectedLegalEntity()).not.toBeInTheDocument()
     })
   })
 
@@ -268,8 +110,8 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} />)
 
-      const label = testUtils.getTitleFilterLabel()
-      const input = testUtils.getTitleInput()
+      const label = warehouseListFilterTestUtils.getTitleFilterLabel()
+      const input = warehouseListFilterTestUtils.getTitleInput()
 
       expect(label).toBeInTheDocument()
       expect(input).toBeInTheDocument()
@@ -284,7 +126,7 @@ describe('Фильтр списка складов', () => {
       const { user } = render(<WarehouseListFilter {...props} />)
 
       const value = fakeWord()
-      const input = await testUtils.setTitle(user, value)
+      const input = await warehouseListFilterTestUtils.setTitle(user, value)
 
       expect(input).toHaveDisplayValue(value)
     })
@@ -296,7 +138,7 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} formValues={{ title: titleValue }} />)
 
-      const input = testUtils.getTitleInput()
+      const input = warehouseListFilterTestUtils.getTitleInput()
       expect(input).toHaveDisplayValue(titleValue)
     })
 
@@ -307,9 +149,9 @@ describe('Фильтр списка складов', () => {
       const { user } = render(<WarehouseListFilter {...props} />)
 
       const value = fakeWord()
-      await testUtils.setTitle(user, value)
-      await testUtils.resetTitle(user)
-      const input = testUtils.getTitleInput()
+      await warehouseListFilterTestUtils.setTitle(user, value)
+      await warehouseListFilterTestUtils.resetTitle(user)
+      const input = warehouseListFilterTestUtils.getTitleInput()
 
       expect(input).not.toHaveDisplayValue(value)
     })
@@ -322,10 +164,10 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} />)
 
-      await testUtils.expectLegalEntityLoadingFinished()
-      const label = testUtils.getLegalEntityFilterLabel()
-      const select = testUtils.getLegalEntitySelect()
-      const selectedLegalEntity = testUtils.getSelectedLegalEntity()
+      await warehouseListFilterTestUtils.expectLegalEntityLoadingFinished()
+      const label = warehouseListFilterTestUtils.getLegalEntityFilterLabel()
+      const select = warehouseListFilterTestUtils.getLegalEntitySelect()
+      const selectedLegalEntity = warehouseListFilterTestUtils.getSelectedLegalEntity()
 
       expect(label).toBeInTheDocument()
       expect(select).toBeInTheDocument()
@@ -340,10 +182,10 @@ describe('Фильтр списка складов', () => {
 
       const { user } = render(<WarehouseListFilter {...props} />)
 
-      await testUtils.expectLegalEntityLoadingFinished()
-      await testUtils.openLegalEntitySelect(user)
-      await testUtils.setLegalEntity(user, legalEntityListItem.title)
-      const selectedLegalEntity = testUtils.getSelectedLegalEntity()
+      await warehouseListFilterTestUtils.expectLegalEntityLoadingFinished()
+      await warehouseListFilterTestUtils.openLegalEntitySelect(user)
+      await warehouseListFilterTestUtils.setLegalEntity(user, legalEntityListItem.title)
+      const selectedLegalEntity = warehouseListFilterTestUtils.getSelectedLegalEntity()
 
       expect(selectedLegalEntity).toBeInTheDocument()
       expect(selectedLegalEntity).toHaveTextContent(legalEntityListItem.title)
@@ -358,8 +200,8 @@ describe('Фильтр списка складов', () => {
         <WarehouseListFilter {...props} formValues={{ legalEntity: legalEntityListItem.id }} />,
       )
 
-      await testUtils.expectLegalEntityLoadingFinished()
-      const selectedLegalEntity = testUtils.getSelectedLegalEntity()
+      await warehouseListFilterTestUtils.expectLegalEntityLoadingFinished()
+      const selectedLegalEntity = warehouseListFilterTestUtils.getSelectedLegalEntity()
 
       expect(selectedLegalEntity).toBeInTheDocument()
       expect(selectedLegalEntity).toHaveTextContent(legalEntityListItem.title)
@@ -372,11 +214,11 @@ describe('Фильтр списка складов', () => {
 
       const { user } = render(<WarehouseListFilter {...props} />)
 
-      await testUtils.expectLegalEntityLoadingFinished()
-      await testUtils.openLegalEntitySelect(user)
-      await testUtils.setLegalEntity(user, legalEntityListItem.title)
-      await testUtils.resetLegalEntity(user)
-      const selectedLegalEntity = testUtils.getSelectedLegalEntity()
+      await warehouseListFilterTestUtils.expectLegalEntityLoadingFinished()
+      await warehouseListFilterTestUtils.openLegalEntitySelect(user)
+      await warehouseListFilterTestUtils.setLegalEntity(user, legalEntityListItem.title)
+      await warehouseListFilterTestUtils.resetLegalEntity(user)
+      const selectedLegalEntity = warehouseListFilterTestUtils.getSelectedLegalEntity()
 
       expect(selectedLegalEntity).not.toBeInTheDocument()
     })
@@ -389,8 +231,8 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} />)
 
-      const label = testUtils.getAddressFilterLabel()
-      const input = testUtils.getAddressInput()
+      const label = warehouseListFilterTestUtils.getAddressFilterLabel()
+      const input = warehouseListFilterTestUtils.getAddressInput()
 
       expect(label).toBeInTheDocument()
       expect(input).toBeInTheDocument()
@@ -405,7 +247,7 @@ describe('Фильтр списка складов', () => {
       const { user } = render(<WarehouseListFilter {...props} />)
 
       const value = fakeWord()
-      const input = await testUtils.setAddress(user, value)
+      const input = await warehouseListFilterTestUtils.setAddress(user, value)
 
       expect(input).toHaveDisplayValue(value)
     })
@@ -417,7 +259,7 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} formValues={{ address: addressValue }} />)
 
-      const input = testUtils.getAddressInput()
+      const input = warehouseListFilterTestUtils.getAddressInput()
       expect(input).toHaveDisplayValue(addressValue)
     })
 
@@ -428,9 +270,9 @@ describe('Фильтр списка складов', () => {
       const { user } = render(<WarehouseListFilter {...props} />)
 
       const value = fakeWord()
-      await testUtils.setAddress(user, value)
-      await testUtils.resetAddress(user)
-      const input = testUtils.getAddressInput()
+      await warehouseListFilterTestUtils.setAddress(user, value)
+      await warehouseListFilterTestUtils.resetAddress(user)
+      const input = warehouseListFilterTestUtils.getAddressInput()
 
       expect(input).not.toHaveDisplayValue(value)
     })
@@ -443,10 +285,10 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} />)
 
-      await testUtils.expectParentLoadingFinished()
-      const label = testUtils.getParentFilterLabel()
-      const select = testUtils.getParentSelect()
-      const selectedParent = testUtils.getSelectedParent()
+      await warehouseListFilterTestUtils.expectParentLoadingFinished()
+      const label = warehouseListFilterTestUtils.getParentFilterLabel()
+      const select = warehouseListFilterTestUtils.getParentSelect()
+      const selectedParent = warehouseListFilterTestUtils.getSelectedParent()
 
       expect(label).toBeInTheDocument()
       expect(select).toBeInTheDocument()
@@ -461,10 +303,10 @@ describe('Фильтр списка складов', () => {
 
       const { user } = render(<WarehouseListFilter {...props} />)
 
-      await testUtils.expectParentLoadingFinished()
-      await testUtils.openParentSelect(user)
-      await testUtils.setParent(user, warehouseListItem.title)
-      const selectedParent = testUtils.getSelectedParent()
+      await warehouseListFilterTestUtils.expectParentLoadingFinished()
+      await warehouseListFilterTestUtils.openParentSelect(user)
+      await warehouseListFilterTestUtils.setParent(user, warehouseListItem.title)
+      const selectedParent = warehouseListFilterTestUtils.getSelectedParent()
 
       expect(selectedParent).toBeInTheDocument()
       expect(selectedParent).toHaveTextContent(warehouseListItem.title)
@@ -477,8 +319,8 @@ describe('Фильтр списка складов', () => {
 
       render(<WarehouseListFilter {...props} formValues={{ parent: warehouseListItem.id }} />)
 
-      await testUtils.expectParentLoadingFinished()
-      const selectedParent = testUtils.getSelectedParent()
+      await warehouseListFilterTestUtils.expectParentLoadingFinished()
+      const selectedParent = warehouseListFilterTestUtils.getSelectedParent()
 
       expect(selectedParent).toBeInTheDocument()
       expect(selectedParent).toHaveTextContent(warehouseListItem.title)
@@ -491,11 +333,11 @@ describe('Фильтр списка складов', () => {
 
       const { user } = render(<WarehouseListFilter {...props} />)
 
-      await testUtils.expectParentLoadingFinished()
-      await testUtils.openParentSelect(user)
-      await testUtils.setParent(user, warehouseListItem.title)
-      await testUtils.resetParent(user)
-      const selectedParent = testUtils.getSelectedParent()
+      await warehouseListFilterTestUtils.expectParentLoadingFinished()
+      await warehouseListFilterTestUtils.openParentSelect(user)
+      await warehouseListFilterTestUtils.setParent(user, warehouseListItem.title)
+      await warehouseListFilterTestUtils.resetParent(user)
+      const selectedParent = warehouseListFilterTestUtils.getSelectedParent()
 
       expect(selectedParent).not.toBeInTheDocument()
     })
