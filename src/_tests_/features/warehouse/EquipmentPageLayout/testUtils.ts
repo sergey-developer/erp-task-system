9eb33a17af766fb51c9ecbce394b80836ee1582f
Александр Меньшиков <a.menshikov@rsvn.ru>
2024-09-24 17:36:09 +0300

    Вынес testUtils для EquipmentPageLayout

---
+++ src/_tests_/features/warehouse/EquipmentPageLayout/testUtils.ts
@@ -0,0 +1,39 @@
+import { screen, within } from '@testing-library/react'
+import { UserEvent } from '@testing-library/user-event/setup/setup'
+
+import { buttonTestUtils } from '_tests_/utils/index'
+
+import { TestIdsEnum } from './constants'
+
+const getContainer = () => screen.getByTestId(TestIdsEnum.EquipmentPageLayout)
+
+// filter button
+const getFilterButton = () => buttonTestUtils.getButtonIn(getContainer(), /filter/)
+
+const clickFilterButton = async (user: UserEvent) => {
+  const button = getFilterButton()
+  await user.click(button)
+}
+
+// search field
+const getSearchField = () => within(getContainer()).getByPlaceholderText('Поиск оборудования')
+
+const setSearch = async (
+  user: UserEvent,
+  value: string,
+  pressEnter: boolean = false,
+): Promise<HTMLElement> => {
+  const field = getSearchField()
+  await user.type(field, pressEnter ? value.concat('{enter}') : value)
+  return field
+}
+
+export const equipmentPageLayoutTestUtils = {
+  getContainer,
+
+  getFilterButton,
+  clickFilterButton,
+
+  getSearchField,
+  setSearch,
+}
