ce962544eefb17dcd1a4f6e2c77cd3f62ed5a0d6
s.sergeev <s.sergeev@rsvn.ru>
2024-10-07 11:25:35 +0300

    правки слияния с epic-26425

--- src/_tests_/fixtures/infrastructures/index.ts
+++ src/_tests_/fixtures/infrastructures/index.ts
@@ -4,7 +4,6 @@ import * as infrastructureStatusHistory from './infrastructureStatusHistory'
 import * as infrastructureWork from './infrastructureWork'
 import * as infrastructureWorkType from './infrastructureWorkType'
 import * as infrastructureWorkTypeCost from './infrastructureWorkTypeCost'
-import * as urgencyRateType from './urgencyRateType'
 
 const infrastructuresFixtures = {
   ...infrastructure,
